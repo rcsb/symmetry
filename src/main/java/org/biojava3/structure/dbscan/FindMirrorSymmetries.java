5dcadf87d58f67570fc6b0b0febddc9918390339
Andreas Prlic <andreas.prlic@gmail.com>
2011-02-19 23:43:19 +0000

    starting to work on Mirror Symmetry DB scan
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@11553 c7567c6c-0293-4223-8362-9e6c12f08bf1

---
+++ src/main/java/org/biojava3/structure/dbscan/FindMirrorSymmetries.java
@@ -0,0 +1,96 @@
+package org.biojava3.structure.dbscan;
+
+import java.util.SortedSet;
+
+import org.biojava.bio.structure.Atom;
+import org.biojava.bio.structure.StructureException;
+import org.biojava.bio.structure.align.ce.CECalculator;
+import org.biojava.bio.structure.align.ce.CeMain;
+import org.biojava.bio.structure.align.ce.CeParameters;
+import org.biojava.bio.structure.align.model.AFPChain;
+import org.biojava.bio.structure.align.util.AFPChainScorer;
+import org.biojava.bio.structure.align.util.AtomCache;
+import org.biojava.bio.structure.jama.Matrix;
+import org.rcsb.fatcat.server.PdbChainKey;
+
+public class FindMirrorSymmetries {
+	public static void main(String[] args){
+		SortedSet<PdbChainKey> reps = GetRepresentatives.getRepresentatives();
+		AtomCache cache = new AtomCache("/Users/ap3/WORK/PDB/",true);
+		FindMirrorSymmetries me = new FindMirrorSymmetries();
+
+		int total = 0;
+		int symmetric = 0;
+		for ( PdbChainKey r : reps){
+			try {
+				String name = r.toName();
+				Atom[] ca1 = cache.getAtoms(name);
+				Atom[] ca2 = cache.getAtoms(name);
+
+				AFPChain afp = me.align(ca1,ca2,name);
+				afp.setAlgorithmName(CeMain.algorithmName);
+
+				//boolean isSignificant =  afp.isSignificantResult();
+				boolean isSignificant = false;
+				if ( afp.getTMScore() > 0.3 && afp.isSignificantResult())
+					isSignificant = true;
+				total++;
+				if ( isSignificant)
+					symmetric++;
+				System.out.print(name + "\t" + String.format("%.2f", afp.getProbability()) + String.format(" %.2f", afp.getTMScore()));
+				System.out.println(" " + symmetric + "/" + total + " (" + (symmetric/(float)total*100f)+"%)");
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private AFPChain align(Atom[] ca1, Atom[] ca2, String name) throws StructureException {
+		int rows = ca1.length ;
+		int cols = ca2.length ;
+
+		CeParameters params = new CeParameters();
+		params.setCheckCircular(true);
+		CECalculator calculator = new CECalculator(params);
+
+		//Build alignment ca1 to ca2-ca2
+		AFPChain afpChain = new AFPChain();
+		afpChain.setName1(name);
+		afpChain.setName2(name);
+		afpChain = calculator.extractFragments(afpChain, ca1, ca2);
+
+		Matrix origM = new Matrix( calculator.getMatMatrix());
+		// symmetry hack, disable main diagonale
+
+		for ( int i = 0 ; i< rows; i++){
+			for ( int j = 0 ; j < cols ; j++){
+				int diff = Math.abs(i-j);
+
+				if ( diff < 15 ){
+					origM.set(i,j, 99);
+				}
+				int diff2 = Math.abs(i-(j-ca2.length/2));
+				if ( diff2 < 15 ){
+					origM.set(i,j, 99);
+				}
+			}
+		}
+
+		//showMatrix(origM, "original CE matrix");
+		calculator.setMatMatrix(origM.getArray());
+		//calculator.setMatMatrix(diffDistMax.getArray());
+		calculator.traceFragmentMatrix( afpChain,ca1, ca2);
+		calculator.nextStep( afpChain,ca1, ca2);
+
+		afpChain.setAlgorithmName("SpeedCE");
+		afpChain.setVersion("0.0000001");
+
+
+		double tmScore = AFPChainScorer.getTMScore(afpChain, ca1, ca2);
+		afpChain.setTMScore(tmScore);
+
+		return afpChain;
+
+
+	}
+}

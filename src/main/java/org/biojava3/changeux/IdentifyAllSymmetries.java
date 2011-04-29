bdee95d2199d5deba22694b4324a687f846f4848
Andreas Prlic <andreas.prlic@gmail.com>
2011-04-29 22:43:28 +0000

    latest code to display pairwise symmetry
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@12467 c7567c6c-0293-4223-8362-9e6c12f08bf1

--- src/main/java/org/biojava3/changeux/IdentifyAllSymmetries.java
+++ src/main/java/org/biojava3/changeux/IdentifyAllSymmetries.java
@@ -64,7 +64,7 @@ public class IdentifyAllSymmetries implements MatrixListener {
 		//String name2 = name2;
 
 		IdentifyAllSymmetries me = new IdentifyAllSymmetries();
-
+		me.setDisplayJmol(true);
 		List<AFPChain> results = me.indentifyAllSymmetries(name1, name2, cache, fragmentLength,progress);
 		
 		int pos = -1;
@@ -224,7 +224,7 @@ public class IdentifyAllSymmetries implements MatrixListener {
 	public static  boolean isSignificant(AFPChain myAFP){
 
 		//|| 
-		return ( (myAFP.getTMScore() >= 0.35  || myAFP.getProbability() >= 3.5 ) && myAFP.getTotalRmsdOpt() < 5.0);
+		return ( (myAFP.getTMScore() >= 0.35  && myAFP.getProbability() >= 3.5 ) && myAFP.getTotalRmsdOpt() < 5.0);
 	}
 
 	private void showCurrentAlig(AFPChain myAFP, Atom[] ca12, Atom[] ca22) throws StructureException {

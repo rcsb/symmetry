b3ca3baaa7d77f9472be5cc1673a8f3a2f041a6a
Peter Rose <pwrose@ucsd.edu>
2011-06-06 22:54:32 +0000

    Added method that returns representative protein chains at a given sequence identity threshold.
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@12678 c7567c6c-0293-4223-8362-9e6c12f08bf1

--- src/main/java/org/biojava3/structure/dbscan/GetRepresentatives.java
+++ src/main/java/org/biojava3/structure/dbscan/GetRepresentatives.java
@@ -1,89 +1,82 @@
 package org.biojava3.structure.dbscan;
 
-
-
 import java.io.InputStream;
 
 import java.net.URL;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.SortedSet;
 
 import java.util.TreeSet;
-
-
 
 import org.biojava.bio.structure.align.client.JFatCatClient;
 
 import org.biojava.bio.structure.align.util.HTTPConnectionTools;
 
-
-
 import org.biojava.bio.structure.align.xml.RepresentativeXMLConverter;
 
 import org.rcsb.fatcat.server.PdbChainKey;
 
-
-
 public class GetRepresentatives {
 
-	static final String url = "http://www.rcsb.org/pdb/rest/representatives?cluster=40";
+	private static String coreUrl = "http://www.rcsb.org/pdb/rest/representatives?cluster=";
+	// available sequence clusters
+	private static List<Integer> seqIdentities = Arrays.asList(30, 40, 50, 70, 90, 95, 100);
 
+	/**
+	 * Returns a representative set of PDB protein chains at 40% sequence 
+	 * identity cutoff.
+	 * @param sequenceIdentity
+	 * @return PdbChainKey set of representatives
+	 * @deprecated
+	 */
+	public static SortedSet<PdbChainKey> getRepresentatives() {
+        return getRepresentatives(40);
+	}
 
-
-	public static SortedSet<PdbChainKey> getRepresentatives(){
-
+	/**
+	 * Returns a representative set of PDB protein chains at the specified sequence 
+	 * identity cutoff. See http://www.pdb.org/pdb/statistics/clusterStatistics.do
+	 * for more information.
+	 * @param sequenceIdentity sequence identity threshold
+	 * @return PdbChainKey set of representatives
+	 */
+	public static SortedSet<PdbChainKey> getRepresentatives(int sequenceIdentity) {
 		SortedSet<PdbChainKey> representatives = new TreeSet<PdbChainKey>();
+
+		if (!seqIdentities.contains(sequenceIdentity)) {
+			System.err.println("Error: representative chains are not available for %sequence identity: "
+							+ sequenceIdentity);
+			return representatives;
+		}
+
 
 		try {
 
-			URL u = new URL(url);
+			URL u = new URL(coreUrl + sequenceIdentity);
 
-			InputStream stream = HTTPConnectionTools.getInputStream(u,15000);
-
-
+			InputStream stream = HTTPConnectionTools.getInputStream(u, 15000);
 
 			String xml = null;
 
-
-
-			if ( stream != null) {
-
-
-
+			if (stream != null) {
 				xml = JFatCatClient.convertStreamToString(stream);
-
-
-
-
-
-
 
 				SortedSet<String> reps = RepresentativeXMLConverter.fromXML(xml);
 
-
-
-				for (String s : reps){
-
-					PdbChainKey k =  PdbChainKey.fromName(s);
-
+				for (String s : reps) {
+					PdbChainKey k = PdbChainKey.fromName(s);
 					representatives.add(k);
-
 				}
 
 			}
 
-		} catch(Exception e){
-
+		} catch (Exception e) {
 			e.printStackTrace();
-
 		}
 
 		return representatives;
-
-
-
 	}
 
 }
-
-

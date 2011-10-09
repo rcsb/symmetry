78f3faf06d56e83c940e72d4f56a1a5340c2e1e3
Andreas Prlic <andreas.prlic@gmail.com>
2011-10-09 04:32:01 +0000

    cleanup
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@13934 c7567c6c-0293-4223-8362-9e6c12f08bf1

--- src/test/java/org/biojava3/benchmark/TestFisherBenchmark.java
+++ src/test/java/org/biojava3/benchmark/TestFisherBenchmark.java
@@ -1,19 +1,10 @@
 package org.biojava3.benchmark;
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
-
 import junit.framework.TestCase;
-
-import org.biojava.bio.structure.Atom;
-import org.biojava.bio.structure.align.StructureAlignment;
-import org.biojava.bio.structure.align.StructureAlignmentFactory;
-import org.biojava.bio.structure.align.ce.CeMain;
 import org.biojava.bio.structure.align.client.PdbPair;
-import org.biojava.bio.structure.align.model.AFPChain;
-import org.biojava.bio.structure.align.model.AfpChainWriter;
-import org.biojava.bio.structure.align.util.AtomCache;
+
 
 public class TestFisherBenchmark extends TestCase {
 

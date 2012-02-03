1a740e5c209717f584e41e24ddc1ad9b877a6ec3
Peter Rose <pwrose@ucsd.edu>
2012-02-03 18:55:27 +0000

    Added methods to get representatives at different levels of sequence identity
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@14706 c7567c6c-0293-4223-8362-9e6c12f08bf1

--- src/main/java/org/biojava3/structure/dbscan/GetRepresentatives.java
+++ src/main/java/org/biojava3/structure/dbscan/GetRepresentatives.java
@@ -1,6 +1,8 @@
 package org.biojava3.structure.dbscan;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import java.net.URL;
 
@@ -20,7 +22,9 @@ import org.rcsb.fatcat.server.PdbChainKey;
 
 public class GetRepresentatives {
 
-	private static String coreUrl = "http://www.rcsb.org/pdb/rest/representatives?cluster=";
+	private static String clusterUrl = "http://www.rcsb.org/pdb/rest/representatives?cluster=";
+	private static String allUrl = "http://www.rcsb.org/pdb/rest/getCurrent/";
+
 	// available sequence clusters
 	private static List<Integer> seqIdentities = Arrays.asList(30, 40, 50, 70, 90, 95, 100);
 
@@ -54,7 +58,7 @@ public class GetRepresentatives {
 
 		try {
 
-			URL u = new URL(coreUrl + sequenceIdentity);
+			URL u = new URL(clusterUrl + sequenceIdentity);
 
 			InputStream stream = HTTPConnectionTools.getInputStream(u, 60000);
 
@@ -78,5 +82,38 @@ public class GetRepresentatives {
 
 		return representatives;
 	}
+	
+	/**
+	 * Returns the current list of all PDB IDs.
+	 * @return PdbChainKey set of all PDB IDs.
+	 */
+	public static SortedSet<String> getAll() {
+		SortedSet<String> representatives = new TreeSet<String>();
 
+		try {
+
+			URL u = new URL(allUrl);
+
+			InputStream stream = HTTPConnectionTools.getInputStream(u, 60000);
+
+			if (stream != null) {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(stream));
+
+				String line = null;
+
+				while ((line = reader.readLine()) != null) {
+					int index = line.lastIndexOf("structureId=");
+					if (index > 0) {
+						representatives.add(line.substring(index + 13, index + 17));
+					}
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return representatives;
+	}
 }

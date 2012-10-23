5250bb9bbffcdb095f25ca909813c3f182ccaa8d
Andreas Prlic <andreas.prlic@gmail.com>
2012-10-23 23:40:53 +0000

    does not depend on sequenceui
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@17203 c7567c6c-0293-4223-8362-9e6c12f08bf1

--- src/main/java/org/biojava3/structure/sequenceui/TestAllChains.java
+++ src/main/java/org/biojava3/structure/sequenceui/TestAllChains.java
@@ -2,26 +2,16 @@ package org.biojava3.structure.sequenceui;
 
 import java.util.SortedSet;
 
-import javax.swing.JFrame;
 
 import org.biojava3.structure.dbscan.GetRepresentatives;
 import org.rcsb.fatcat.server.PdbChainKey;
-import org.rcsb.sequence.biojavadao.BioJavaPubMedFactory;
-import org.rcsb.sequence.biojavadao.BioJavaResidueInfoFactory;
-import org.rcsb.sequence.biojavadao.BioJavaSequenceCollectionFactory;
-import org.rcsb.sequence.core.PubMedProvider;
-import org.rcsb.sequence.core.ResidueProvider;
-import org.rcsb.sequence.core.SequenceCollectionFactory;
-import org.rcsb.sequence.core.SequenceCollectionProvider;
-import org.rcsb.sequence.model.ResidueInfoFactory;
-import org.rcsb.sequence.util.ImageWrapper;
 
 
 public class TestAllChains {
 
 	public static void main(String[] args){
 
-		initBioJavaView();
+		//initBioJavaView();
 
 		SortedSet<PdbChainKey> reps = GetRepresentatives.getRepresentatives();
 
@@ -30,9 +20,9 @@ public class TestAllChains {
 			counter++;
 			try {
 				System.out.println("#"  + counter + " " + rep);
-				JFrame frame = showChain(rep);
+				//JFrame frame = showChain(rep);
 
-				frame.dispose();
+				//frame.dispose();
 			} catch (Exception e){
 				e.printStackTrace();
 				System.err.println("Could not display " + rep);
@@ -43,29 +33,29 @@ public class TestAllChains {
 
 	}
 
-	private static JFrame showChain(PdbChainKey rep) { 
-
-		return ImageWrapper.showSeq(rep.getPdbId(), rep.getChainId());
-
-
-	}
-
-	/** provide a default view using BioJava.. could be done using some proper configuration managment...
-	 * 
-	 */
-	public static void initBioJavaView(){
-
-		// first the Residue Provider
-		ResidueInfoFactory refactory = new BioJavaResidueInfoFactory();
-		ResidueProvider.setResidueInfoFactory(refactory);
-
-		// next the SequenceCollection Provider
-		SequenceCollectionFactory sfactory = new BioJavaSequenceCollectionFactory();
-		SequenceCollectionProvider.setSequenceCollectionFactory(sfactory);
-
-		BioJavaPubMedFactory pfactory = new BioJavaPubMedFactory();
-		PubMedProvider.setPubMedFactory(pfactory);
-
-
-	}
+//	private static JFrame showChain(PdbChainKey rep) { 
+//
+//		return ImageWrapper.showSeq(rep.getPdbId(), rep.getChainId());
+//
+//
+//	}
+//
+//	/** provide a default view using BioJava.. could be done using some proper configuration managment...
+//	 * 
+//	 */
+//	public static void initBioJavaView(){
+//
+//		// first the Residue Provider
+//		ResidueInfoFactory refactory = new BioJavaResidueInfoFactory();
+//		ResidueProvider.setResidueInfoFactory(refactory);
+//
+//		// next the SequenceCollection Provider
+//		SequenceCollectionFactory sfactory = new BioJavaSequenceCollectionFactory();
+//		SequenceCollectionProvider.setSequenceCollectionFactory(sfactory);
+//
+//		BioJavaPubMedFactory pfactory = new BioJavaPubMedFactory();
+//		PubMedProvider.setPubMedFactory(pfactory);
+//
+//
+//	}
 }

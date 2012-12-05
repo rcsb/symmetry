6721397546458a08969c2e51d09b26c5263202e8
Peter Rose <pwrose@ucsd.edu>
2012-12-05 07:02:58 +0000

    Added check for number of protein chains. If no protein chains are found, calculation is terminated (i.e., for DNA PDB 166D)
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@17726 c7567c6c-0293-4223-8362-9e6c12f08bf1

--- src/main/java/demo/DemoOrientBioAssembly.java
+++ src/main/java/demo/DemoOrientBioAssembly.java
@@ -35,8 +35,7 @@ import org.biojava3.structure.quaternary.analysis.CalcBioAssemblySymmetry;
 public class DemoOrientBioAssembly {
 	public static void main(String[] args){
 
-	//	String pdbID = "4hhb";
-		String pdbID = "1bhc";
+		String pdbID = "4HHB";
 		int  biolAssemblyNr = 1;
 
 		Structure s;
@@ -76,60 +75,70 @@ public class DemoOrientBioAssembly {
 		calc.setBioAssembly(s);
 
 		calc.getParams().setSequenceIdentityThreshold(threshold);
-		
-		calc.orient();
-		
-		String jmolScript = calc.getScriptGenerator().playOrientations();
 
-		String script = "set defaultStructureDSSP true; set measurementUnits ANGSTROMS;  select all;  spacefill off; wireframe off; " +
-				"backbone off; cartoon on; color cartoon structure; color structure;  select ligand;wireframe 0.16;spacefill 0.5; " +
-				"color cpk ; select all; model 0;set antialiasDisplay true; autobond=false;save STATE state_1;" ;
+		boolean hasProtein = calc.orient();
 
+		if (hasProtein) {
+			String jmolScript = calc.getScriptGenerator().playOrientations();
 
-		StructureAlignmentJmol jmol = new StructureAlignmentJmol();
-		jmol.setStructure(s);
-
-		String title = "Symmetry results for " + pdbID + " bio assembly: " + biolAssemblyNr + " seq cutoff:" + calc.getParams().getSequenceIdentityThreshold();
-		jmol.setTitle(title);
-		
-		
-		jmol.evalString(script);
+			String script = "set defaultStructureDSSP true; set measurementUnits ANGSTROMS;  select all;  spacefill off; wireframe off; " +
+					"backbone off; cartoon on; color cartoon structure; color structure;  select ligand;wireframe 0.16;spacefill 0.5; " +
+					"color cpk ; select all; model 0;set antialiasDisplay true; autobond=false;save STATE state_1;" ;
 
 
-		jmol.evalString(jmolScript);
+			StructureAlignmentJmol jmol = new StructureAlignmentJmol();
+			jmol.setStructure(s);
 
-		
-		// items for database:
-		
-		System.out.println("=================");
-		System.out.println(title );
-		System.out.println("=================");
-		System.out.println("Sequence ID   : " + calc.getParams().getSequenceIdentityThreshold() );
-		System.out.println("Stoichiometry : " + calc.getFinder().getCompositionFormula());
-		System.out.println("Point Group   : " + calc.getRotationGroup().getPointGroup()	);
-		System.out.println("Pseudosymmetry: " + calc.getFinder().isPseudoSymmetric());
-		System.out.println("Symmetry RMSD : " + String.format("%.2f",calc.getRotationGroup().getAverageTraceRmsd()));
-	
-		System.out.println("Transf. matrix: " + calc.getAxisTransformation().getTransformation());
-		
-		System.out.println("Dimension                : " + calc.getAxisTransformation().getDimension());
-		
-		System.out.println("Color by subunit         : " + calc.getScriptGenerator().colorBySubunit());
-		System.out.println("Color by sequence cluster: " + calc.getScriptGenerator().colorBySequenceCluster());
-		System.out.println("Color by symmetry        : " + calc.getScriptGenerator().colorBySymmetry());
-		
-		System.out.println("Draw axes                : " + calc.getScriptGenerator().drawAxes());
-		System.out.println("Draw polyhedron          : " + calc.getScriptGenerator().drawPolyhedron());
-		
-		System.out.println("Default orientation      : " + calc.getScriptGenerator().setDefaultOrientation());
-		System.out.println("Orientation count        : " + calc.getScriptGenerator().getOrientationCount());
-		for (int i = 0; i <  calc.getScriptGenerator().getOrientationCount(); i++) {
-			System.out.println("Orientation name " + i + "       : " + calc.getScriptGenerator().getOrientationName(i));
-			System.out.println("Orientation " + i + "            : " + calc.getScriptGenerator().setOrientation(i));
+			String title = "Symmetry results for " + pdbID + " bio assembly: " + biolAssemblyNr + " seq cutoff:" + calc.getParams().getSequenceIdentityThreshold();
+			jmol.setTitle(title);
+
+
+			jmol.evalString(script);
+
+
+			jmol.evalString(jmolScript);
+
+
+			// items for database:
+
+			System.out.println("=================");
+			System.out.println(title );
+			System.out.println("=================");
+			System.out.println("Sequence ID   : " + calc.getParams().getSequenceIdentityThreshold() );
+			System.out.println("Stoichiometry : " + calc.getFinder().getCompositionFormula());
+			System.out.println("Point Group   : " + calc.getRotationGroup().getPointGroup()	);
+			System.out.println("Pseudosymmetry: " + calc.getFinder().isPseudoSymmetric());
+			System.out.println("Symmetry RMSD : " + String.format("%.2f",calc.getRotationGroup().getAverageTraceRmsd()));
+
+			System.out.println("Transf. matrix: " + calc.getAxisTransformation().getTransformation());
+
+			System.out.println("Dimension                : " + calc.getAxisTransformation().getDimension());
+
+			System.out.println("Color by subunit         : " + calc.getScriptGenerator().colorBySubunit());
+			System.out.println("Color by subunit length  : " + calc.getScriptGenerator().colorBySubunit().length());
+			System.out.println("Color by sequence cluster: " + calc.getScriptGenerator().colorBySequenceCluster());
+			System.out.println("Color by seq. clst. len  : " + calc.getScriptGenerator().colorBySequenceCluster().length());
+			System.out.println("Color by symmetry        : " + calc.getScriptGenerator().colorBySymmetry());
+			System.out.println("Color by symmetry length : " + calc.getScriptGenerator().colorBySymmetry().length());
+
+			System.out.println("Draw axes                : " + calc.getScriptGenerator().drawAxes());
+			System.out.println("Draw axes length         : " + calc.getScriptGenerator().drawAxes().length());
+			System.out.println("Draw polyhedron          : " + calc.getScriptGenerator().drawPolyhedron());
+			System.out.println("Draw polyhedron length   : " + calc.getScriptGenerator().drawPolyhedron().length());
+
+			System.out.println("Default orientation      : " + calc.getScriptGenerator().setDefaultOrientation());
+			System.out.println("Orientation count        : " + calc.getScriptGenerator().getOrientationCount());
+			for (int i = 0; i <  calc.getScriptGenerator().getOrientationCount(); i++) {
+				System.out.println("Orientation name " + i + "       : " + calc.getScriptGenerator().getOrientationName(i));
+				System.out.println("Orientation " + i + "            : " + calc.getScriptGenerator().setOrientation(i));
+			}
+
+			System.out.println("=================");
+			return calc.getFinder().isPseudoSymmetric();
+		} else {
+			System.out.println("No protein chains found");
+			return false;
 		}
-		
-		System.out.println("=================");
-		return calc.getFinder().isPseudoSymmetric();
 	}
 	
 }

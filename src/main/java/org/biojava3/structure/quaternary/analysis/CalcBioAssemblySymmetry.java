3ffd947d8912d0b1325e645d638127271737f04e
Andreas Prlic <andreas.prlic@gmail.com>
2012-12-17 23:16:57 +0000

    playing with version numbers
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@17950 c7567c6c-0293-4223-8362-9e6c12f08bf1

--- src/main/java/org/biojava3/structure/quaternary/analysis/CalcBioAssemblySymmetry.java
+++ src/main/java/org/biojava3/structure/quaternary/analysis/CalcBioAssemblySymmetry.java
@@ -42,7 +42,7 @@ public class CalcBioAssemblySymmetry {
 	private JmolSymmetryScriptGenerator scriptGenerator;
 	
 	
-	public static String version = "0.0.9";
+	public static String version = "0.1.0";
 	
 	public CalcBioAssemblySymmetry(){
 	}

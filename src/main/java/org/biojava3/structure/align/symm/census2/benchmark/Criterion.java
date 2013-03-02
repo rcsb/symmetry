0fa9c10893d3bcd71db9700083b88dd36c73055b
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2013-03-02 21:34:14 +0000

    census2.benchmark.Criterion should fail gracefully.
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/StructCompPlayground/trunk@18922 c7567c6c-0293-4223-8362-9e6c12f08bf1

--- src/main/java/org/biojava3/structure/align/symm/census2/benchmark/Criterion.java
+++ src/main/java/org/biojava3/structure/align/symm/census2/benchmark/Criterion.java
@@ -1,13 +1,15 @@
 package org.biojava3.structure.align.symm.census2.benchmark;
 
+
 public abstract class Criterion<T extends Number> {
 
-	public abstract T get(Case c);
+	public abstract T get(Case c) throws NoncomputableCriterionException;
 
 	public static Criterion<Float> zScore() {
 		return new Criterion<Float>() {
 			@Override
-			public Float get(Case c) {
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAlignment() == null) throw new NoncomputableCriterionException("The case has a null getAlignment()");
 				return c.getAlignment().getzScore();
 			}
 		};
@@ -15,7 +17,8 @@ public abstract class Criterion<T extends Number> {
 	public static Criterion<Float> tmScore() {
 		return new Criterion<Float>() {
 			@Override
-			public Float get(Case c) {
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAlignment() == null) throw new NoncomputableCriterionException("The case has a null getAlignment()");
 				return c.getAlignment().getTmScore();
 			}
 		};
@@ -23,7 +26,8 @@ public abstract class Criterion<T extends Number> {
 	public static Criterion<Float> rmsd() {
 		return new Criterion<Float>() {
 			@Override
-			public Float get(Case c) {
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAlignment() == null) throw new NoncomputableCriterionException("The case has a null getAlignment()");
 				return c.getAlignment().getRmsd();
 			}
 		};
@@ -31,7 +35,8 @@ public abstract class Criterion<T extends Number> {
 	public static Criterion<Float> identity() {
 		return new Criterion<Float>() {
 			@Override
-			public Float get(Case c) {
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAlignment() == null) throw new NoncomputableCriterionException("The case has a null getAlignment()");
 				return c.getAlignment().getIdentity();
 			}
 		};
@@ -39,7 +44,8 @@ public abstract class Criterion<T extends Number> {
 	public static Criterion<Float> similarity() {
 		return new Criterion<Float>() {
 			@Override
-			public Float get(Case c) {
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAlignment() == null) throw new NoncomputableCriterionException("The case has a null getAlignment()");
 				return c.getAlignment().getSimilarity();
 			}
 		};
@@ -47,7 +53,8 @@ public abstract class Criterion<T extends Number> {
 	public static Criterion<Float> theta() {
 		return new Criterion<Float>() {
 			@Override
-			public Float get(Case c) {
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAxis() == null) throw new NoncomputableCriterionException("The case has a null getAxis()");
 				return c.getAxis().getTheta();
 			}
 		};
@@ -55,17 +62,33 @@ public abstract class Criterion<T extends Number> {
 	public static Criterion<Float> thetaIsCorrect() {
 		return new Criterion<Float>() {
 			@Override
-			public Float get(Case c) {
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAxis() == null) throw new NoncomputableCriterionException("The case has a null getAxis()");
+				if (c.getOrder() == null) throw new NoncomputableCriterionException("The case has a null getOrder()");
 				float theta = c.getAxis().getTheta();
 				float shouldBe = (float) (2*Math.PI / c.getOrder());
 				return Math.abs(shouldBe - theta);
 			}
 		};
 	}
+	public static Criterion<Float> epsilon() {
+		return new Criterion<Float>() {
+			@Override
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAxis() == null) throw new NoncomputableCriterionException("The case has a null getAxis()");
+				if (c.getOrder() == null) throw new NoncomputableCriterionException("The case has a null getOrder()");
+				if (c.getOrder() < 2) throw new NoncomputableCriterionException("The case has a getOrder() of less than 2");
+				Double epsilon = c.getAxis().evaluateEpsilon(c.getOrder());
+				if (epsilon == null) throw new NoncomputableCriterionException("Could not determine epsilon");
+				return (float) ((double) epsilon);
+			}
+		};
+	}
 	public static Criterion<Float> screw() {
 		return new Criterion<Float>() {
 			@Override
-			public Float get(Case c) {
+			public Float get(Case c) throws NoncomputableCriterionException {
+				if (c.getAxis() == null) throw new NoncomputableCriterionException("The case has a null getAxis()");
 				return c.getAxis().getScrew();
 			}
 		};
@@ -73,7 +96,8 @@ public abstract class Criterion<T extends Number> {
 	public static Criterion<Integer> alignLength() {
 		return new Criterion<Integer>() {
 			@Override
-			public Integer get(Case c) {
+			public Integer get(Case c) throws NoncomputableCriterionException {
+				if (c.getAlignment() == null) throw new NoncomputableCriterionException("The case has a null getAlignment()");
 				return c.getAlignment().getAlignLength();
 			}
 		};
---
+++ src/main/java/org/biojava3/structure/align/symm/census2/benchmark/NoncomputableCriterionException.java
@@ -0,0 +1,23 @@
+package org.biojava3.structure.align.symm.census2.benchmark;
+
+public class NoncomputableCriterionException extends Exception {
+
+	private static final long serialVersionUID = 5469531500394989349L;
+
+	public NoncomputableCriterionException() {
+		super();
+	}
+
+	public NoncomputableCriterionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public NoncomputableCriterionException(String message) {
+		super(message);
+	}
+
+	public NoncomputableCriterionException(Throwable cause) {
+		super(cause);
+	}
+
+}

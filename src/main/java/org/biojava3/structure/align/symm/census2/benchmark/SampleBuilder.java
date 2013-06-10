d3dfff217a06a7e887fb5eb4ec81aded51067845
dmyersturnbull <dmyersturnbull@gmail.com>
2013-06-10 15:17:34 -0700

    Added usage error and getNames method.

--- src/main/java/org/biojava3/structure/align/symm/census2/benchmark/SampleBuilder.java
+++ src/main/java/org/biojava3/structure/align/symm/census2/benchmark/SampleBuilder.java
@@ -28,9 +28,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.biojava3.structure.align.symm.census2.Result;
 import org.biojava3.structure.align.symm.census2.Results;
 
@@ -40,11 +44,18 @@ import org.biojava3.structure.align.symm.census2.Results;
  */
 public class SampleBuilder {
 
+	private static final Logger logger = LogManager.getLogger(ROCCurves.class.getName());
+
 	public static void main(String[] args) throws IOException {
+		if (args.length != 3) {
+			System.err.println("Usage: " + SampleBuilder.class.getSimpleName() + " input-file output-file groups-file");
+			return;
+		}
 		File input = new File(args[0]);
 		File output = new File(args[1]);
 		File ordersFile = new File(args[2]);
-		buildSample(input, output, getOrders(ordersFile));
+		Map<String, KnownInfo> infos = getOrders(ordersFile);
+		buildSample(input, output, infos);
 	}
 	
 	
@@ -60,9 +71,22 @@ public class SampleBuilder {
 		br.close();
 		return map;
 	}
+
+	public static List<String> getNames(File knownInfoFile) throws IOException {
+		List<String> list = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new FileReader(knownInfoFile));
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			String[] parts = line.split("\t"); // domain tab group
+			list.add(parts[0]);
+		}
+		br.close();
+		return list;
+	}
 	
-	public static void buildSample(File input, File output, Map<String,KnownInfo> knownInfos) throws IOException {
+	public static void buildSample(File input, File output, Map<String, KnownInfo> knownInfos) throws IOException {
 		Results results = Results.fromXML(input);
+		logger.info("File " + input + " contains " + results.size() + " entries");
 		Sample sample = new Sample();
 		for (Result result : results.getData()) {
 			if (result == null) continue;

1796a80eb2ad04ca3d4bd3a5d4c4dde24c95f29d
dmyersturnbull <dmyersturnbull@gmail.com>
2013-07-10 13:28:56 -0700

    Working to make CensusJob play well with web-based calls, yet not store AFPChain. This commit calls a method in biojava-structure that's currently only in my fork.

--- src/main/java/org/biojava3/structure/align/symm/census2/CensusJob.java
+++ src/main/java/org/biojava3/structure/align/symm/census2/CensusJob.java
@@ -1,24 +1,19 @@
 /*
- *                    BioJava development code
- *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  If you do not have a copy,
- * see:
- *
- *      http://www.gnu.org/copyleft/lesser.html
- *
- * Copyright for this code is held jointly by the individual
- * authors.  These should be listed in @author doc comments.
- *
- * For more information on the BioJava project and its aims,
- * or to join the biojava-l mailing list, visit the home page
+ * BioJava development code
+ * 
+ * This code may be freely distributed and modified under the terms of the GNU Lesser General Public Licence. This
+ * should be distributed with the code. If you do not have a copy, see:
+ * 
+ * http://www.gnu.org/copyleft/lesser.html
+ * 
+ * Copyright for this code is held jointly by the individual authors. These should be listed in @author doc comments.
+ * 
+ * For more information on the BioJava project and its aims, or to join the biojava-l mailing list, visit the home page
  * at:
- *
- *      http://www.biojava.org/
- *
+ * 
+ * http://www.biojava.org/
+ * 
  * Created on 2013-02-18
- *
  */
 package org.biojava3.structure.align.symm.census2;
 
@@ -36,7 +31,6 @@ import org.biojava.bio.structure.align.util.AFPChainScorer;
 import org.biojava.bio.structure.align.util.AtomCache;
 import org.biojava.bio.structure.align.util.RotationAxis;
 import org.biojava.bio.structure.jama.Matrix;
-import org.biojava.bio.structure.scop.RemoteScopInstallation;
 import org.biojava.bio.structure.scop.ScopDatabase;
 import org.biojava.bio.structure.scop.ScopDescription;
 import org.biojava.bio.structure.scop.ScopDomain;
@@ -50,30 +44,70 @@ import org.biojava3.structure.align.symm.protodomain.Protodomain;
 
 /**
  * One run of CE-Symm in the census.
+ * 
  * @author dmyerstu
  */
 public class CensusJob implements Callable<Result> {
 
+	public static class WebInfo {
+		private AFPChain afpChain;
+		private SimpleResult result;
+
+		public WebInfo(AFPChain afpChain, SimpleResult result) {
+			super();
+			this.afpChain = afpChain;
+			this.result = result;
+		}
+
+		public AFPChain getAfpChain() {
+			return afpChain;
+		}
+
+		public SimpleResult getResult() {
+			return result;
+		}
+
+		public void setAfpChain(AFPChain afpChain) {
+			this.afpChain = afpChain;
+		}
+
+		public void setResult(SimpleResult result) {
+			this.result = result;
+		}
+	}
+
 	private static final Logger logger = LogManager.getLogger(CensusJob.class.getPackage().getName());
 
+	private AFPChain afpChain;
 	private AlgorithmGiver algorithm;
 
 	private AtomCache cache;
 	private Integer count;
-
 	private ScopDomain domain;
+
+	private ScopDatabase scop;
+
 	private Significance significance;
+
+	private boolean storeAfpChain;
+
 	private ScopDescription superfamily;
 
-	private String scopVersion = null;
+	private Long timeTaken;
 
-	public static Result runOn(ScopDomain domain, AtomCache cache, AlgorithmGiver algorithm, Significance sig, String scopVersion) {
-		ScopDescription superfamily = ScopFactory.getSCOP(scopVersion).getScopDescriptionBySunid(domain.getSuperfamilyId());
-		CensusJob job = new CensusJob(cache, algorithm, sig);
+	/**
+	 * Preferred method for web-based calls.
+	 */
+	public static WebInfo runOn(ScopDomain domain, AtomCache cache, AlgorithmGiver algorithm, Significance sig,
+			ScopDatabase scop) {
+		ScopDescription superfamily = scop.getScopDescriptionBySunid(domain.getSuperfamilyId());
+		CensusJob job = new CensusJob(cache, algorithm, sig, scop);
+		job.setStoreAfpChain(true);
 		job.setCount(0);
 		job.setDomain(domain);
 		job.setSuperfamily(superfamily);
-		return job.call();
+		Result r = job.call();
+		return new WebInfo(job.getAfpChain(), new SimpleResult(r));
 	}
 
 	private static boolean sanityCheckPreAlign(Atom[] ca1, Atom[] ca2) {
@@ -83,43 +117,30 @@ public class CensusJob implements Callable<Result> {
 	}
 
 	public CensusJob(AtomCache cache, AlgorithmGiver algorithm, Significance significance) {
+		this(cache, algorithm, significance, ScopFactory.getSCOP());
+	}
+
+	public CensusJob(AtomCache cache, AlgorithmGiver algorithm, Significance significance, ScopDatabase scop) {
+		this.scop = scop;
 		this.algorithm = algorithm;
 		this.cache = cache;
 		this.significance = significance;
 	}
 
-	private Long timeTaken;
-
-	public Long getTimeTaken() {
-		return timeTaken;
-	}
-
 	@Override
 	public Result call() {
 
-		if (domain == null || superfamily == null || count == null) throw new IllegalStateException (
+		if (domain == null || superfamily == null || count == null) throw new IllegalStateException(
 				"Must set domain, superfamily, and count first.");
 		String name = domain.getScopId();
-
-		if ( scopVersion != null ) {
-			System.out.println("censusJob setting SCOP version to: " + scopVersion);
-			ScopDatabase scop = ScopFactory.getSCOP(scopVersion);
-			ScopFactory.setScopDatabase(scop);
-		}
-
-		// setting SCOP factory to latest BerkeleySCOP
-		if ( ScopFactory.getSCOP() instanceof RemoteScopInstallation) {
-			System.out.println("setting default scop to version " + ScopFactory.VERSION_1_75B);
-			ScopFactory.setScopDatabase(ScopFactory.getSCOP(ScopFactory.VERSION_1_75B));
-		}
 
 		// first, get the atoms
 		Atom[] ca1, ca2;
 		Structure structure;
 		logger.debug("Getting atoms for " + name + " (job #" + count + ")");
 		try {
-			structure = cache.getStructure(name);
-			//			ca1 = cache.getAtoms(name);
+			structure = cache.getStructureForDomain(name, scop);
+			// ca1 = cache.getAtoms(name);
 			ca1 = StructureTools.getAtomCAArray(structure);
 			ca2 = StructureTools.cloneCAArray(ca1);
 		} catch (Exception e) {
@@ -197,17 +218,16 @@ public class CensusJob implements Callable<Result> {
 				logger.error("Failed to determine the signifcance of " + name + ": " + e.getMessage(), e);
 			}
 
-
 			// now find fraction helical
 			Float fractionHelical = null;
-			//			try {
-			//				fractionHelical = getFractionHelical(structure);
-			//			} catch (Exception e) {
-			//				logger.error("Could not assign secondary structure to " + name + " (job #" + count + ")", e);
-			//			}
+			// try {
+			// fractionHelical = getFractionHelical(structure);
+			// } catch (Exception e) {
+			// logger.error("Could not assign secondary structure to " + name + " (job #" + count + ")", e);
+			// }
 
-			return convertResult(afpChain, isSignificant, superfamily, name, order, protodomain.toString(),
-					domain, angle, fractionHelical);
+			return convertResult(afpChain, isSignificant, superfamily, name, order, protodomain.toString(), domain,
+					angle, fractionHelical);
 
 		} else { // trying this would take too long
 			logger.debug("Result is not significant (job #" + count + ")");
@@ -215,31 +235,19 @@ public class CensusJob implements Callable<Result> {
 		}
 	}
 
-	private float getFractionHelical(Structure structure) throws StructureException {
-		SecStruc ss = new SecStruc();
-		ss.assign(structure);
-		SecStrucGroup[] ssgs = ss.getGroups();
-		int nHelix = 0;
-		for (SecStrucGroup ssg : ssgs) {
-			SecStrucState state = (SecStrucState) ssg.getProperty("secstruc");
-			if (state.getSecStruc().isHelixType()) {
-				nHelix++;
-			}
-		}
-		double fractionHelix = (double) nHelix / (double) ssgs.length;
-		return (float) fractionHelix;
+	public AFPChain getAfpChain() {
+		return afpChain;
+	}
+
+	public Long getTimeTaken() {
+		return timeTaken;
 	}
 
 	/**
-	 * Returns the <em>magnitude</em> of the angle between the first and second blocks of {@code afpChain}, measured in degrees. This is always a positive value (unsigned).
-	 * @param afpChain
-	 * @param ca1
-	 * @param ca2
-	 * @return
+	 * Discards the stored AFPChain to free heap memory.
 	 */
-	private double getAngle(AFPChain afpChain, Atom[] ca1, Atom[] ca2) {
-		Matrix rotation = afpChain.getBlockRotationMatrix()[0];
-		return Math.acos(rotation.trace() - 1) * 180/Math.PI;
+	public void nullifyAfpChain() {
+		afpChain = null;
 	}
 
 	public void setCount(int count) {
@@ -250,21 +258,27 @@ public class CensusJob implements Callable<Result> {
 		this.domain = domain;
 	}
 
+	public void setScop(ScopDatabase scop) {
+		this.scop = scop;
+	}
+
+	/**
+	 * Store the raw AFPChain in {@link #getAfpChain()}. Requires a lot of memory. Not recommended.
+	 * 
+	 * @param storeAfpChain
+	 */
+	public void setStoreAfpChain(boolean storeAfpChain) {
+		this.storeAfpChain = storeAfpChain;
+	}
+
 	public void setSuperfamily(ScopDescription superfamily) {
 		this.superfamily = superfamily;
 	}
 
+	private Result convertResult(AFPChain afpChain, Boolean isSymmetric, ScopDescription superfamily, String scopId,
+			Integer order, String protodomain, ScopDomain domain, Float angle, Float fractionHelical) {
 
-
-	public String getScopVersion() {
-		return scopVersion;
-	}
-
-	public void setScopVersion(String scopVersion) {
-		this.scopVersion = scopVersion;
-	}
-
-	private Result convertResult(AFPChain afpChain, Boolean isSymmetric, ScopDescription superfamily, String scopId, Integer order, String protodomain, ScopDomain domain, Float angle, Float fractionHelical) {
+		if (storeAfpChain) this.afpChain = afpChain;
 
 		final String description = superfamily.getDescription();
 
@@ -279,14 +293,13 @@ public class CensusJob implements Callable<Result> {
 
 		r.setAlignment(new Alignment(afpChain));
 		r.setIsSignificant(isSymmetric);
-		//		r.setFractionHelical(fractionHelical);
+		// r.setFractionHelical(fractionHelical);
 		try {
-			if ( afpChain.getAlnLength() > 0 )
-				r.setAxis(new Axis(new RotationAxis(afpChain)));
+			if (afpChain.getAlnLength() > 0) r.setAxis(new Axis(new RotationAxis(afpChain)));
 
 		} catch (RuntimeException e) {
 
-			logger.error("Could not get rotation axis for " + scopId  + "(job #" + count + ")", e);
+			logger.error("Could not get rotation axis for " + scopId + "(job #" + count + ")", e);
 
 		} catch (Exception e) {
 
@@ -310,13 +323,42 @@ public class CensusJob implements Callable<Result> {
 		long startTime = System.currentTimeMillis();
 		AFPChain afpChain = algorithm.getAlgorithm().align(ca1, ca2);
 		long endTime = System.currentTimeMillis();
-		this.timeTaken = endTime - startTime;
+		timeTaken = endTime - startTime;
 		if (afpChain == null) return null;
 		afpChain.setName1(name);
 		afpChain.setName2(name);
 		double realTmScore = AFPChainScorer.getTMScore(afpChain, ca1, ca2);
 		afpChain.setTMScore(realTmScore);
 		return afpChain;
+	}
+
+	/**
+	 * Returns the <em>magnitude</em> of the angle between the first and second blocks of {@code afpChain}, measured in
+	 * degrees. This is always a positive value (unsigned).
+	 * 
+	 * @param afpChain
+	 * @param ca1
+	 * @param ca2
+	 * @return
+	 */
+	private double getAngle(AFPChain afpChain, Atom[] ca1, Atom[] ca2) {
+		Matrix rotation = afpChain.getBlockRotationMatrix()[0];
+		return Math.acos(rotation.trace() - 1) * 180 / Math.PI;
+	}
+
+	private float getFractionHelical(Structure structure) throws StructureException {
+		SecStruc ss = new SecStruc();
+		ss.assign(structure);
+		SecStrucGroup[] ssgs = ss.getGroups();
+		int nHelix = 0;
+		for (SecStrucGroup ssg : ssgs) {
+			SecStrucState state = (SecStrucState) ssg.getProperty("secstruc");
+			if (state.getSecStruc().isHelixType()) {
+				nHelix++;
+			}
+		}
+		double fractionHelix = (double) nHelix / (double) ssgs.length;
+		return (float) fractionHelix;
 	}
 
 }

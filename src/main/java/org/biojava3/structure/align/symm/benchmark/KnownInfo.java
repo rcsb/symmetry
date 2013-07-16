e54dc43dd3788f3056b8db0e6314968ad1a8bc7f
dmyersturnbull <dmyersturnbull@gmail.com>
2013-07-16 14:28:23 -0700

    Created BenchmarkSetStats.

--- src/main/java/org/biojava3/structure/align/symm/benchmark/KnownInfo.java
+++ src/main/java/org/biojava3/structure/align/symm/benchmark/KnownInfo.java
@@ -1,24 +1,19 @@
 /*
- *                    BioJava development code
- *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  If you do not have a copy,
- * see:
- *
- *      http://www.gnu.org/copyleft/lesser.html
- *
- * Copyright for this code is held jointly by the individual
- * authors.  These should be listed in @author doc comments.
- *
- * For more information on the BioJava project and its aims,
- * or to join the biojava-l mailing list, visit the home page
+ * BioJava development code
+ * 
+ * This code may be freely distributed and modified under the terms of the GNU Lesser General Public Licence. This
+ * should be distributed with the code. If you do not have a copy, see:
+ * 
+ * http://www.gnu.org/copyleft/lesser.html
+ * 
+ * Copyright for this code is held jointly by the individual authors. These should be listed in @author doc comments.
+ * 
+ * For more information on the BioJava project and its aims, or to join the biojava-l mailing list, visit the home page
  * at:
- *
- *      http://www.biojava.org/
- *
+ * 
+ * http://www.biojava.org/
+ * 
  * Created on 2013-03-03
- *
  */
 package org.biojava3.structure.align.symm.benchmark;
 
@@ -27,13 +22,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * A known (true) space group and corresponding order of symmetry.
- * Can differentiate between different types of symmetry.
+ * A known (true) space group and corresponding order of symmetry. Can differentiate between different types of
+ * symmetry.
+ * 
  * @author dmyerstu
  */
 public class KnownInfo implements Serializable, Comparable<KnownInfo> {
 	private static final long serialVersionUID = -2667699023747790086L;
 	private String group;
+
+	public static int getOrderFromGroup(String string) {
+		int order = 1;
+		Pattern pattern = Pattern.compile("([\\d])$");
+		Matcher matcher = pattern.matcher(string);
+		boolean found = matcher.find();
+		if (found && matcher.groupCount() == 1) {
+			order = Integer.parseInt(matcher.group(1));
+		}
+		return order;
+	}
 
 	public KnownInfo() {
 
@@ -46,49 +53,37 @@ public class KnownInfo implements Serializable, Comparable<KnownInfo> {
 
 	@Override
 	public int compareTo(KnownInfo o) {
-		if (equals(o))
-			return 0;
-		if (getOrder() < o.getOrder())
-			return -1;
-		if (getOrder() > o.getOrder())
-			return 1;
+		if (equals(o)) return 0;
+		if (getOrder() < o.getOrder()) return -1;
+		if (getOrder() > o.getOrder()) return 1;
 		return group.compareTo(o.getGroup());
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
 		KnownInfo other = (KnownInfo) obj;
 		if (group == null) {
-			if (other.group != null)
-				return false;
-		} else if (!group.equals(other.group))
-			return false;
+			if (other.group != null) return false;
+		} else if (!group.equals(other.group)) return false;
 		return true;
 	}
 
 	public String getGroup() {
 		return group;
 	}
-	
-	public static int getOrderFromGroup(String string) {
-		int order = 1;
-		Pattern pattern = Pattern.compile("([\\d])$");
-		Matcher matcher = pattern.matcher(string);
-		boolean found = matcher.find();
-		if (found && matcher.groupCount() == 1) {
-			order = Integer.parseInt(matcher.group(1));
-		}
-		return order;
-	}
-	
+
 	public int getOrder() {
 		return getOrderFromGroup(group);
+	}
+
+	/**
+	 * @return Whether this info has <em>cyclic but not dihedral</em> symmetry.
+	 */
+	public boolean hasCyclicSymmetry() {
+		return !isAsymmetric() && group.contains("C");
 	}
 
 	/**
@@ -96,6 +91,14 @@ public class KnownInfo implements Serializable, Comparable<KnownInfo> {
 	 */
 	public boolean hasDihedralSymmetry() {
 		return !isAsymmetric() && group.contains("D");
+	}
+
+	/**
+	 * @return Whether this info has <em>rotational or true helical</em> symmetry and its order of symmetry is an even
+	 *         number.
+	 */
+	public boolean hasEvenOrderSymmetry() {
+		return (hasRotationalSymmetry() || hasTrueHelicalSymmetry()) && getOrder() % 2 == 0;
 	}
 
 	@Override
@@ -108,16 +111,27 @@ public class KnownInfo implements Serializable, Comparable<KnownInfo> {
 
 	/**
 	 * @return Whether this info <em>has helical but not</em> rotational symmetry.
+	 *         <em>This includes superhelical and non-integral order helical symmetry.</em>
 	 */
 	public boolean hasHelicalSymmetry() {
 		return !isAsymmetric() && group.contains("H");
 	}
 
 	/**
-	 * @return Whether this info has rotational symmetry and its order of symmetry is an odd number.
+	 * @return Whether this info has helical symmetry for which the symmetry operation must be applied a fractional
+	 *         number of times to result in the trivial alignment.
+	 *         <em>This includes superhelical and non-integral order helical symmetry.</em>
+	 */
+	public boolean hasNonIntegralOrderSymmetry() {
+		return group.equals("NIH");
+	}
+
+	/**
+	 * @return Whether this info has <em>rotational or true helical</em> symmetry and its order of symmetry is an odd
+	 *         number.
 	 */
 	public boolean hasOddOrderSymmetry() {
-		return hasRotationalSymmetry() && getOrder() % 2 == 1;
+		return (hasRotationalSymmetry() || hasTrueHelicalSymmetry()) && getOrder() % 2 == 1;
 	}
 
 	/**
@@ -128,10 +142,26 @@ public class KnownInfo implements Serializable, Comparable<KnownInfo> {
 	}
 
 	/**
+	 * @return Whether this info has a "bent" or curved helical symmetry, such as that of a screw that is bent around in
+	 *         a curve.
+	 */
+	public boolean hasSuperhelicalSymmetry() {
+		return group.equals("SH");
+	}
+
+	/**
 	 * @return Whether this info <em>has translational but not</em> rotational symmetry.
 	 */
 	public boolean hasTranslationalSymmetry() {
 		return !isAsymmetric() && group.contains("R");
+	}
+
+	/**
+	 * @return Whether this info <em>has <strong>true</strong> helical symmetry</em>. This <em>does not</em> include
+	 *         superhelical or non-integral order helical symmetry.
+	 */
+	public boolean hasTrueHelicalSymmetry() {
+		return hasHelicalSymmetry() && !hasSuperhelicalSymmetry() && !hasNonIntegralOrderSymmetry();
 	}
 
 	/**
@@ -149,4 +179,5 @@ public class KnownInfo implements Serializable, Comparable<KnownInfo> {
 	public String toString() {
 		return group;
 	}
+
 }
---
+++ src/main/java/org/biojava3/structure/align/symm/benchmark/set/BenchmarkSetStats.java
@@ -0,0 +1,121 @@
+package org.biojava3.structure.align.symm.benchmark.set;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import org.biojava3.structure.align.symm.benchmark.KnownInfo;
+import org.biojava3.structure.align.symm.benchmark.SampleBuilder;
+import org.biojava3.structure.align.symm.census2.stats.StatUtils;
+
+/**
+ * Finds basic statistics about the benchmark set itself (known info).
+ * @author dmyerstu
+ * @since 0.2.2
+ */
+public class BenchmarkSetStats {
+
+	private int nCyclic;
+	private int nDihedral;
+	private int nEven;
+	private int nHelical;
+	private int nNonIntegralHelical;
+	private int nOdd;
+	private int nSuperhelical;
+	private int nSymm;
+	private int nTotal;
+	private int nTranslational;
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args.length > 1) {
+			System.err.println("Usage: " + BenchmarkSetStats.class.getSimpleName() + " [file]");
+			return;
+		}
+		File file = new File("src/main/resources/domain_symm_benchmark.tsv");
+		if (args.length > 0) {
+			file = new File(args[0]);
+		}
+		BenchmarkSetStats stats = new BenchmarkSetStats(file);
+		System.out.println(stats);
+	}
+
+	public BenchmarkSetStats(File file) throws IOException {
+		this(SampleBuilder.getOrders(file));
+	}
+
+	public BenchmarkSetStats(LinkedHashMap<String, KnownInfo> orders) {
+		for (KnownInfo info : orders.values()) {
+			if (info.hasDihedralSymmetry()) nDihedral++;
+			if (info.hasTranslationalSymmetry()) nTranslational++;
+			if (info.hasCyclicSymmetry()) nCyclic++;
+			if (info.hasTrueHelicalSymmetry()) nHelical++;
+			if (info.hasOddOrderSymmetry()) nOdd++;
+			if (info.hasEvenOrderSymmetry()) nEven++;
+			if (!info.isAsymmetric()) nSymm++;
+			if (info.hasSuperhelicalSymmetry()) nSuperhelical++;
+			if (info.hasNonIntegralOrderSymmetry()) nNonIntegralHelical++;
+		}
+		nTotal = orders.size();
+	}
+
+	public int getnCyclic() {
+		return nCyclic;
+	}
+
+	public int getnDihedral() {
+		return nDihedral;
+	}
+
+	public int getnEven() {
+		return nEven;
+	}
+
+	public int getnHelical() {
+		return nHelical;
+	}
+
+	public int getnNonIntegralHelical() {
+		return nNonIntegralHelical;
+	}
+
+	public int getnOdd() {
+		return nOdd;
+	}
+
+	public int getnSuperhelical() {
+		return nSuperhelical;
+	}
+
+	public int getnSymm() {
+		return nSymm;
+	}
+
+	public int getnTotal() {
+		return nTotal;
+	}
+
+	public int getnTranslational() {
+		return nTranslational;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("symmetric\t" + nSymm + StatUtils.NEWLINE);
+		sb.append("cyclic\t" + nCyclic + StatUtils.NEWLINE);
+		sb.append("dihedral\t" + nDihedral + StatUtils.NEWLINE);
+		sb.append("translational\t" + nTranslational + StatUtils.NEWLINE);
+		sb.append("true helical\t" + (nHelical - nSuperhelical - nNonIntegralHelical) + StatUtils.NEWLINE);
+		sb.append("superhelical\t" + nSuperhelical + StatUtils.NEWLINE);
+		sb.append("non-integral helical\t" + nNonIntegralHelical + StatUtils.NEWLINE);
+		sb.append("even-order\t" + nEven + StatUtils.NEWLINE);
+		sb.append("odd-order\t" + nOdd + StatUtils.NEWLINE);
+		sb.append("total\t" + nTotal + StatUtils.NEWLINE);
+		return sb.toString();
+	}
+
+}

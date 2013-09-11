9f490fcf23130a823d23b7910ccf22e6439d4113
dmyersturnbull <dmyersturnbull@gmail.com>
2013-09-10 22:26:03 -0700

    Fixed bugs in FoldOrder.

--- src/main/java/org/biojava3/structure/align/symm/census2/stats/FoldOrder.java
+++ src/main/java/org/biojava3/structure/align/symm/census2/stats/FoldOrder.java
@@ -30,11 +30,14 @@ public class FoldOrder {
 		private Map<String, Integer> hasOrderMap = new HashMap<String, Integer>();
 		private Map<String, Map<Integer,Integer>> ordersMap = new HashMap<String, Map<Integer,Integer>>();
 		private Map<String, Float> tmScoreMap = new HashMap<String, Float>();
+		private Map<String, Integer> counts = new HashMap<String, Integer>();
 
 		public void add(Result result) {
 
 			String fold = normalizer.group(result);
 
+			StatUtils.plus(counts, fold);
+			
 			if (result.getAlignment() != null) {
 				StatUtils.plusF(tmScoreMap, fold, result.getAlignment().getTmScore());
 			}
@@ -42,14 +45,17 @@ public class FoldOrder {
 			int order = 1;
 			if (result.getOrder() != null) {
 				order = result.getOrder();
+				if (order < 0) order = 1; // it would be unfair to decrease the score
 				StatUtils.plus(hasOrderMap, fold, order > 1 ? 1 : 0);
 			}
 
+			if (ordersMap.get(fold) == null) {
+				ordersMap.put(fold, new HashMap<Integer,Integer>(7));
+			}
 			if (result.getAlignment() != null && result.getAlignment().getTmScore() >= tmScoreCutoff && order > 1) {
-				if (ordersMap.get(fold) == null) {
-					ordersMap.put(fold, new HashMap<Integer,Integer>(7));
-				}
 				StatUtils.plus(ordersMap.get(fold), order);
+			} else {
+//				StatUtils.plus(ordersMap.get(fold), 1);
 			}
 
 		}
@@ -69,8 +75,13 @@ public class FoldOrder {
 		public int getConsensusOrder(String fold) {
 
 			// if asymmetric, return 1
-			double meanTmScore = (double) tmScoreMap.get(fold) / tmScoreMap.size();
-			double meanHasOrder = (double) hasOrderMap.get(fold) / hasOrderMap.size();
+			int n = counts.get(fold);
+			Float tmScore = tmScoreMap.get(fold);
+			if (tmScore == null) tmScore = 0.0f;
+			Integer order = hasOrderMap.get(fold);
+			if (order == null) order = 1;
+			double meanTmScore = (double) tmScore / n;
+			double meanHasOrder = (double) order / n;
 			if (meanTmScore < tmScoreCutoff || meanHasOrder < orderCutoff) {
 				return 1;
 			}
@@ -78,8 +89,9 @@ public class FoldOrder {
 			// otherwise, return the mode order
 			int maxCount = 0;
 			int maximizingOrder = 0;
-			for (int i = 1; i < ordersMap.get(fold).size(); i++) { // could also start at 0 or 2
-				int count = ordersMap.get(fold).get(i);
+			for (int i = 2; i <= 8; i++) {
+				Integer count = ordersMap.get(fold).get(i);
+				if (count == null) count = 0;
 				if (count > maxCount) {
 					maxCount = count;
 					maximizingOrder = i;
@@ -97,11 +109,15 @@ public class FoldOrder {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: " + SymmetryOrder.class.getSimpleName() + " census-file.xml");
+		if (args.length < 1 || args.length > 2) {
+			System.err.println("Usage: " + SymmetryOrder.class.getSimpleName() + " census-file.xml [grouping]");
 			return;
 		}
-		FoldOrder orders = new FoldOrder(Results.fromXML(new File(args[0])));
+		FoldOrder orders = new FoldOrder();
+		if (args.length > 1) {
+			orders.setNormalizer(Grouping.byName(args[1]));
+		}
+		orders.run(Results.fromXML(new File(args[0])));
 		System.out.println(orders);
 	}
 
@@ -110,11 +126,11 @@ public class FoldOrder {
 	// just always call this "fold" in the code
 	private Grouping normalizer = Grouping.fold();
 
-	private double orderCutoff = 0.01;
+	private double orderCutoff = 0.00;
 
-	private double tmScoreCutoff = 0.01;
+	private double tmScoreCutoff = 0.4;
 
-	public FoldOrder(Results census) {
+	public void run(Results census) {
 
 		OrderDecider decider = new OrderDecider();
 		ScopDatabase scop = ScopFactory.getSCOP(ScopFactory.VERSION_1_75A);
@@ -122,13 +138,18 @@ public class FoldOrder {
 		Map<String, Integer> nDomainsInFolds = new HashMap<String, Integer>();
 
 		for (Result result : census.getData()) {
-			ScopDomain domain = scop.getDomainByScopID(result.getScopId());
-			if (domain == null) {
-				logger.error(result.getScopId() + " is null");
+			try {
+				ScopDomain domain = scop.getDomainByScopID(result.getScopId());
+				if (domain == null) {
+					logger.error(result.getScopId() + " is null");
+				}
+				result.setClassification(domain.getClassificationId());
+				String fold = normalizer.group(result);
+				StatUtils.plus(nDomainsInFolds, fold);
+				decider.add(result);
+			} catch (RuntimeException e) {
+				logger.warn("Failed on " + result.getScopId(), e);
 			}
-			String fold = normalizer.group(result);
-			StatUtils.plus(nDomainsInFolds, fold);
-			decider.add(result);
 		}
 
 		for (String fold : nDomainsInFolds.keySet()) {

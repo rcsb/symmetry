d52c1b4616d6f887f183114ceed082e19102708e
dmyersturnbull <dmyersturnbull@gmail.com>
2013-10-19 16:20:42 -0700

    Updated centroid ligand-finding tests. Made LigandFinder able to work with structures other than domains.

--- src/test/java/org/biojava3/structure/align/symm/census2/analysis/LigandFinderTest.java
+++ src/test/java/org/biojava3/structure/align/symm/census2/analysis/LigandFinderTest.java
@@ -1,6 +1,5 @@
 package org.biojava3.structure.align.symm.census2.analysis;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -15,6 +14,7 @@ import org.biojava3.structure.align.symm.census2.Census;
 import org.biojava3.structure.align.symm.census2.CensusJob;
 import org.biojava3.structure.align.symm.census2.Result;
 import org.biojava3.structure.align.symm.census2.Results;
+import org.biojava3.structure.align.symm.census2.Significance;
 import org.biojava3.structure.align.symm.census2.SignificanceFactory;
 import org.junit.Before;
 import org.junit.Test;
@@ -41,21 +41,31 @@ public class LigandFinderTest {
 		// 4-helix bundles with each heme situated in center
 		String[] scopIds = new String[] {"d1hmda_", "d1hmdb_", "d1hmdc_", "d1hmdd_"};
 		for (String scopId : scopIds) {
-			String ligand = find(scopId, RADIUS);
-			assertNotNull(ligand);
+			String ligand = find(scopId, RADIUS, false, SignificanceFactory.forCeSymmOrd());
+			assertNotNull("Found no ligand for " + scopId, ligand);
 			assertTrue(ligand.contains("Fe2O"));
 		}
 	}
 
 	@Test
+	public void testNotNearAxis1() {
+		// ligand is near centroid but not near axis
+		String name = "2VR1.B"; // could try 4BCT
+		String centroid = find(name, 5, false, SignificanceFactory.ultraLiberal());
+		assertNotNull(centroid);
+		String symmetry = find(name, 5, true, SignificanceFactory.ultraLiberal());
+		assertNull(symmetry);
+	}
+	
+	@Test
 	public void testNearAndFar() {
 		// A transcriptional regulator (from paper)
 		String scopId = "d3ddva1";
-		String ligand = find(scopId, 10);
+		String ligand = find(scopId, 10, false, SignificanceFactory.forCeSymmOrd());
 		assertNotNull(ligand);
 		assertFalse(ligand.contains(","));
 		assertTrue(ligand.contains("Mg"));
-		ligand = find(scopId, RADIUS);
+		ligand = find(scopId, RADIUS, false, SignificanceFactory.forCeSymmOrd());
 		assertNull(ligand);
 	}
 
@@ -63,7 +73,7 @@ public class LigandFinderTest {
 	public void testNotInCenter1() {
 		// callogen; ligands are actually pretty close to centroid
 		String scopId = "d1caga_";
-		String ligand = find(scopId, RADIUS);
+		String ligand = find(scopId, RADIUS, false, SignificanceFactory.forCeSymmOrd());
 		assertNull(ligand);
 	}
 
@@ -71,7 +81,7 @@ public class LigandFinderTest {
 	public void testNotInCenter2() {
 		// a 4-helix bundle with a Zinc ligand outside
 		String scopId = "d1a0ba_";
-		String ligand = find(scopId, RADIUS);
+		String ligand = find(scopId, RADIUS, false, SignificanceFactory.forCeSymmOrd());
 		assertNull(ligand);
 	}
 
@@ -79,7 +89,7 @@ public class LigandFinderTest {
 	public void testNotInCenter3() {
 		// symmetry along interface (from paper)
 		String scopId = "d1squa_";
-		String ligand = find(scopId, RADIUS);
+		String ligand = find(scopId, RADIUS, false, SignificanceFactory.forCeSymmOrd());
 		assertNull(ligand);
 	}
 
@@ -87,38 +97,45 @@ public class LigandFinderTest {
 	public void testHybrid() {
 		// An organic sulfate is in the center, but a hydrocarbon is just outside
 		String scopId = "d3ejba1";
-		String ligand = find(scopId, RADIUS);
+		String ligand = find(scopId, RADIUS, false, SignificanceFactory.forCeSymmOrd());
 		assertNotNull(ligand);
 		assertFalse(ligand.contains(","));
 		assertTrue(ligand.contains("C8O5S"));
 	}
 
 	@Test
+	public void testHybridOnlyAligned() {
+		String scopId = "d3ejba1";
+		String ligand = find(scopId, RADIUS, true, SignificanceFactory.forCeSymmOrd());
+		assertNull(ligand);
+	}
+
+	@Test
 	public void testNoLigand() {
 		// a TIM barrel with no ligand
 		String scopId = "d1ypia_";
-		String ligand = find(scopId, RADIUS);
+		String ligand = find(scopId, RADIUS, false, SignificanceFactory.forCeSymmOrd());
 		assertNull(ligand);
 	}
 
 	@Test
 	public void testAsymmetric() {
-		String scopId = "d1a19a_";
-		String ligand = find(scopId, RADIUS);
+		String scopId = "d1hmda_";
+		String ligand = find(scopId, RADIUS, false, SignificanceFactory.not(SignificanceFactory.ultraLiberal()));
 		assertNull(ligand);
 	}
 	
-	private String find(String scopId, int radius) {
-		CensusJob job = CensusJob.forScopId(Census.AlgorithmGiver.getDefault(), SignificanceFactory.rotationallySymmetricSmart(), scopId, 0, cache, scop);
-		Result result = job.call();
+	private String find(String name, int radius, boolean useOnlyAligned, Significance sig) {
+		Result result = CensusJob.runJob(name, 0, Census.AlgorithmGiver.getDefault(), SignificanceFactory.rotationallySymmetricSmart(), cache, scop);
 		System.out.println(result);
-		if (!SignificanceFactory.rotationallySymmetricSmart().isSignificant(result)) return null;
 		Results results = new Results();
 		results.add(result);
 		LigandFinder finder = new LigandFinder(radius);
+		finder.setUseOnlyAligned(useOnlyAligned);
+		finder.setSignificance(sig);
 		finder.find(results);
 		Map<String,String> formulas = finder.getFormulas();
-		if (("[" + scopId + "]").equals(formulas.get(scopId))) return null;
-		return formulas.get(scopId);
+		if (("[" + name + "]").equals(formulas.get(name))) return null;
+		return formulas.get(name);
 	}
 }

d1fbaf01288bd307f62863a0e0795eed99cf2783
dmyersturnbull <dmyersturnbull@gmail.com>
2013-10-30 22:14:15 -0700

    Wrote a test for InterfaceLigandFinder, which currently doesn't work.

---
+++ src/test/java/org/biojava3/structure/align/symm/census2/analysis/InterfaceLigandFinderTest.java
@@ -0,0 +1,48 @@
+package org.biojava3.structure.align.symm.census2.analysis;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.biojava.bio.structure.align.util.AtomCache;
+import org.biojava.bio.structure.scop.ScopDatabase;
+import org.biojava.bio.structure.scop.ScopFactory;
+import org.biojava3.structure.align.symm.census2.Census;
+import org.biojava3.structure.align.symm.census2.CensusJob;
+import org.biojava3.structure.align.symm.census2.Result;
+import org.biojava3.structure.align.symm.census2.Results;
+import org.biojava3.structure.align.symm.census2.Significance;
+import org.biojava3.structure.align.symm.census2.SignificanceFactory;
+import org.junit.Test;
+
+/**
+ * A test for {@link InterfaceLigandFinder}.
+ * @author dmyersturnbull
+ */
+public class InterfaceLigandFinderTest {
+
+	private AtomCache cache = new AtomCache();
+	private ScopDatabase scop = ScopFactory.getSCOP(ScopFactory.VERSION_1_75B);
+
+	@Test
+	public void nearInCenterOfAxis() {
+		String name = "1hmd.A";
+		String ligand = find(name, 5, false, SignificanceFactory.ultraLiberal());
+		assertNotNull(ligand);
+	}
+
+	private String find(String name, int radius, boolean useOnlyAligned, Significance sig) {
+		Result result = CensusJob.runJob(name, 0, Census.AlgorithmGiver.getDefault(), SignificanceFactory.rotationallySymmetricSmart(), cache, scop);
+		System.out.println(result);
+		Results results = new Results();
+		results.add(result);
+		InterfaceLigandFinder finder = new InterfaceLigandFinder();
+		finder.setMaxDistance(radius);
+		finder.setSignificance(sig);
+		finder.find(results);
+		Map<String,String> formulas = finder.getFormulas();
+		if (("[" + name + "]").equals(formulas.get(name))) return null;
+		return formulas.get(name);
+	}
+}

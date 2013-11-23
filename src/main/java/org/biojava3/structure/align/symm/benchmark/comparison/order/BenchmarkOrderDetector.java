2c02a55f7a9e61ff8e3e508fefccf3e0a1211486
Spencer Bliven <sbliven@ucsd.edu>
2013-11-22 18:28:11 -0800

    RotationOrderDetector improvements
    Add optional output file to save results

--- src/main/java/org/biojava3/structure/align/symm/benchmark/comparison/order/BenchmarkOrderDetector.java
+++ src/main/java/org/biojava3/structure/align/symm/benchmark/comparison/order/BenchmarkOrderDetector.java
@@ -2,15 +2,19 @@ package org.biojava3.structure.align.symm.benchmark.comparison.order;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import org.biojava.bio.structure.align.util.AtomCache;
+import org.biojava.bio.structure.scop.ScopFactory;
 import org.biojava3.structure.align.symm.benchmark.Case;
 import org.biojava3.structure.align.symm.benchmark.Sample;
 import org.biojava3.structure.align.symm.census2.Result;
 import org.biojava3.structure.align.symm.census2.SignificanceFactory;
 import org.biojava3.structure.align.symm.census2.stats.StatUtils;
+import org.biojava3.structure.align.symm.order.AngleOrderDetector;
 import org.biojava3.structure.align.symm.order.OrderDetector;
 import org.biojava3.structure.align.symm.order.RotationOrderDetector;
+import org.biojava3.structure.align.symm.order.SequenceFunctionOrderDetector;
 
 /**
  * Test the performance of a {@link OrderDetector} against a benchmark.
@@ -19,31 +23,78 @@ import org.biojava3.structure.align.symm.order.RotationOrderDetector;
 public class BenchmarkOrderDetector {
 
 	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: " + BenchmarkOrderDetector.class.getSimpleName() + " input-benchmark-file");
+		if (args.length < 1 || args.length >2) {
+			System.err.println("Usage: " + BenchmarkOrderDetector.class.getSimpleName() + " input-benchmark-file [output-file]");
 			return;
 		}
-		Sample sample = Sample.fromXML(new File(args[0]));
+		String inputFilename = args[0];
+		String outputFilename = null;
+		if(args.length>1) {
+			outputFilename = args[1];
+		}
+
+
+		OrderDetector detector;
+		detector = new RotationOrderDetector();
+		//detector = new AngleOrderDetector(0.1);
+		detector = new SequenceFunctionOrderDetector();
+
+		// Read in benchmark
+		ScopFactory.setScopDatabase(ScopFactory.VERSION_1_75A, true);
+		Sample sample = Sample.fromXML(new File(inputFilename));
 		BenchmarkOrderDetector b = new BenchmarkOrderDetector(new AtomCache());
-		double acc = b.testOrderDetector(sample, new RotationOrderDetector());
-		System.out.println("accuracy=" + StatUtils.formatP(acc));
+
+		//Set up output
+		PrintStream out = System.out;
+		if(outputFilename != null) {
+			out = new PrintStream(outputFilename);
+		}
+
+		//Recalculate orders
+		b.calculateOrder(sample, detector, out);
+
+		//Calculate accuracy
+		double acc = b.getOrderAccuracy(sample);
+		System.out.println("Accuracy\t"+StatUtils.formatP(acc));
+
+		// Finish up
+		if(out != System.out) {
+			out.close();
+		}
 	}
-	
+
 	private AtomCache cache;
 
 	public BenchmarkOrderDetector(AtomCache cache) {
 		this.cache = cache;
 	}
 
-	public double testOrderDetector(Sample sample, OrderDetector detector) {
+	/**
+	 * Recalculates the order for each result in sample based on the specified
+	 * OrderDetector
+	 * @param sample
+	 * @param detector
+	 * @param out An output stream to report results (may be null)
+	 */
+	public void calculateOrder(Sample sample, OrderDetector detector, PrintStream out) {
 		OrderDetermination determination = new OrderDetectorDeterminationAdaptor(detector, cache);
 		for (Case c : sample.getData()) {
 			Result result = c.getResult();
-			result.setOrder(determination.getOrder(result));
+			int order = determination.getOrder(result);
+			if(out != null) {
+				out.println(result.getScopId()+"\t"+order);
+			}
+			result.setOrder(order);
 		}
+	}
+
+	/**
+	 * Calculates the accuracy of the order detection
+	 * @param sample
+	 * @return
+	 */
+	public double getOrderAccuracy(Sample sample ) {
 		OrderAccuracy acc = new OrderAccuracy(sample, SignificanceFactory.generallySymmetric(), GroupComparisonFactory.exact());
 		return acc.getAccuracy();
 	}
-
-	
 }

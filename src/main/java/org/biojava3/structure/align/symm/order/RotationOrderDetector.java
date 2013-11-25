42d7bd519c06a418d70266ed5b1140cfb95409bd
Spencer Bliven <sbliven@ucsd.edu>
2013-11-25 15:52:33 -0800

    Adding second detection method within RotationOrderDetector

--- src/main/java/org/biojava3/structure/align/symm/order/RotationOrderDetector.java
+++ src/main/java/org/biojava3/structure/align/symm/order/RotationOrderDetector.java
@@ -21,6 +21,8 @@ public class RotationOrderDetector implements OrderDetector {
 
 	private int maxOrder;
 
+	private final double angleIncr = 5*Calc.radiansPerDegree; // angular resolution
+
 	public RotationOrderDetector() {
 		this(8);
 	}
@@ -32,6 +34,10 @@ public class RotationOrderDetector implements OrderDetector {
 
 	@Override
 	public int calculateOrder(AFPChain afpChain, Atom[] ca) throws OrderDetectionFailedException {
+		return calculateOrderHarmonics(afpChain,ca);
+	}
+
+	int calculateOrderHarmonics(AFPChain afpChain, Atom[] ca) throws OrderDetectionFailedException {
 
 		try {
 
@@ -46,21 +52,51 @@ public class RotationOrderDetector implements OrderDetector {
 			double[] coefficients = fitHarmonics(ca,axis);
 
 			// Find order with maximum weight
-			double bestScore = coefficients[0];
-			int argmax = 1;
-			for(int i=1;i<coefficients.length;i++) {
-				if(coefficients[i] > bestScore ) {
-					argmax = i+1;
-					bestScore = coefficients[i];
+			// ignore initial intercept term
+			double bestScore = coefficients[1];
+			int maxorder = 1;
+			for(int order=2;order<coefficients.length;order++) {
+				if(coefficients[order] > bestScore ) {
+					maxorder = order;
+					bestScore = coefficients[order];
 				}
 			}
-			return argmax;
+			return maxorder;
 
 		} catch (Exception e) {
 			throw new OrderDetectionFailedException(e);
 		}
 	}
+	int calculateOrderHarmonicsFloating(AFPChain afpChain, Atom[] ca) throws OrderDetectionFailedException {
 
+		try {
+
+			RotationAxis axis = new RotationAxis(afpChain);
+
+			// Use C1 order if the axis is undefined
+			if(!axis.isDefined()) {
+				return 1;
+			}
+			// Calculate weights for each order
+			//TODO only use aligned residues, rather than the whole ca
+			double[] coefficients = fitHarmonicsFloating(ca,axis);
+
+			// Find order with maximum weight
+			// ignore initial intercept term
+			double bestScore = coefficients[1];
+			int maxorder = 1;
+			for(int order=2;order<coefficients.length;order++) {
+				if(coefficients[order] > bestScore ) {
+					maxorder = order;
+					bestScore = coefficients[order];
+				}
+			}
+			return maxorder;
+
+		} catch (Exception e) {
+			throw new OrderDetectionFailedException(e);
+		}
+	}
 	/**
 	 * Provide a rough alignment-free metric for the similarity between two
 	 * superimposed structures.
@@ -72,7 +108,7 @@ public class RotationOrderDetector implements OrderDetector {
 	 * @return the average distance to the closest atom
 	 * @throws StructureException if an error occurs finding distances between atoms
 	 */
-	public double superpositionDistance(Atom[] ca1, Atom[] ca2) throws StructureException {
+	public static double superpositionDistance(Atom[] ca1, Atom[] ca2) throws StructureException {
 
 		// Store the closest distance yet found
 		double[] bestDist1 = new double[ca1.length];
@@ -103,38 +139,37 @@ public class RotationOrderDetector implements OrderDetector {
 		double dist = total/(ca1.length+ca2.length);
 		return dist;
 	}
-	
+
 	/**
 	 * Models the relationship between order and superpositionDistance as a
 	 * combination of sine squared functions:
 	 * 
-	 *  f(angle) = a1*sin(angle/2)^2 + a2*sin(2*angle/2)^2 + a3*sin(3*angle/2)^2 + ...
+	 *  f(angle) = 0 + a1*sin(angle/2)^2 + a2*sin(2*angle/2)^2 + a3*sin(3*angle/2)^2 + ...
 	 *  
 	 * Performs a best-fit to find coefficients a1,a2,...,aMaxOrder.
 	 * These give the relative contribution of each order to the overall function.
 	 * @param ca Aligned residues from the protein structure
 	 * @param axis The rotaton axis about which to rotate
-	 * @return an array of length maxOrder giving the coefficients
+	 * @return an array of length maxOrder+1 giving the intercept (0) and coefficients a1...a_maxOrder
 	 * @throws StructureException
 	 */
-	public double[] fitHarmonics(Atom[] ca, RotationAxis axis) throws StructureException {
-		final double angleIncr = 5*Calc.radiansPerDegree;
+	double[] fitHarmonics(Atom[] ca, RotationAxis axis) throws StructureException {
 		final int steps = (int)Math.floor(Math.PI/angleIncr);
 
 		// Fit (angle,distance) points to the following equation
 		// f(angle) = a1*sin^2(1*angle/2) + a2*sin^2(2*angle/2) +...+ a*sin2(maxOrder*angle/2)
 		// goal is to find a_1...a_maxOrder
-		
+
 		double[][] distances = new double[steps][1];
 		// holds the sin2(i*angle/2) terms
 		double[][] harmonics = new double[steps][maxOrder];
-		
+
 		Atom[] ca2 = StructureTools.cloneCAArray(ca);
-		
+
 		for (int step=0; step<steps;step++) {
 			double dist = superpositionDistance(ca, ca2);
 			distances[step][0] = dist;
-			
+
 			for(int order=0;order<maxOrder;order++) {
 				double angle = angleIncr*step;
 				double x = Math.sin( (order+1)*angle/2);
@@ -143,48 +178,115 @@ public class RotationOrderDetector implements OrderDetector {
 			// Rotate for next step
 			axis.rotate(ca2, angleIncr);
 		}
-		
+
 		Matrix y = new Matrix(distances);
 		Matrix M = new Matrix(harmonics);
 		//y=y.getMatrix(1, steps-1, 0, 0);
 		//M=M.getMatrix(1, steps-1, 1, maxOrder-1);
-		
-		
+
+
 		Matrix A = M.transpose().times(M).times(2);
 		Matrix b = M.transpose().times(y).times(2);
-		Matrix c = y.transpose().times(y);
-		
+		//Matrix c = y.transpose().times(y);
+
 		// f(x) = x'Ax/2-bx+c
 		// f'(x) = Ax-b = 0
 		// Ax = b
-//		assert(A.getRowDimension() == maxOrder);
-//		assert(A.getColumnDimension() == maxOrder);
-//		assert(b.getRowDimension() == 1);
-//		assert(b.getColumnDimension() == maxOrder);
-//		assert(c.getRowDimension() == 1);
-//		assert(c.getColumnDimension() == 1);
-		
-		A.inverse();
-		
+
+		//A.inverse();
+
 		Matrix harmonicWeights = A.solve(b);
-		
-//		assert(harmonicWeights.getRowDimension() == maxOrder);
-//		assert(harmonicWeights.getColumnDimension() == 1);
+
+		double[] coefs = new double[maxOrder+1];
+		coefs[0] = 0.;
+		for(int i=0;i<maxOrder;i++) {
+			coefs[i+1] = harmonicWeights.get( i, 0);
+		}
+		return coefs;
+	}
+
+	/**
+	 * Like fitHarmonics, but adds an intercept term and ignores points too close
+	 * to angle 0, which artificially is forced to zero.
+	 * 
+	 *  f(angle) = a0 + a1*sin(angle/2)^2 + a2*sin(2*angle/2)^2 + a3*sin(3*angle/2)^2 + ...
+	 *  
+	 * Performs a best-fit to find coefficients a1,a2,...,aMaxOrder.
+	 * These give the relative contribution of each order to the overall function.
+	 * @param ca Aligned residues from the protein structure
+	 * @param axis The rotaton axis about which to rotate
+	 * @return an array of length maxOrder+1 giving the coefficients a0...a_maxOrder
+	 *  (intercept is element 0)
+	 * @throws StructureException
+	 */
+	double[] fitHarmonicsFloating(Atom[] ca, RotationAxis axis) throws StructureException {
+		// Range of angles to use for training
+		final double minAngle = Math.floor(Math.PI/maxOrder/angleIncr)*angleIncr; // first valid peak
+		final double maxAngle = Math.PI;
+		// Number of angle steps
+		final int steps = (int)Math.floor((maxAngle-minAngle)/angleIncr);
+
+		// Fit (angle,distance) points to the following equation
+		// f(angle) = a0 + a1*sin^2(1*angle/2) + a2*sin^2(2*angle/2) +...+ a*sin2(maxOrder*angle/2)
+		// goal is to find a_1...a_maxOrder
+
+		double[][] distances = new double[steps][1];//preserve matrix dimensions
+		// holds the sin2(i*angle/2) terms
+		double[][] harmonics = new double[steps][maxOrder+1];
+
+		Atom[] ca2 = StructureTools.cloneCAArray(ca);
+
+		if(minAngle != 0.) {
+			axis.rotate(ca2, minAngle);
+		}
+		for (int step=0; step<steps;step++) {
+			double dist = superpositionDistance(ca, ca2);
+			distances[step][0] = dist;
+
+			//initialize intercept column
+			harmonics[step][0] = 1.;
+
+			//later columns
+			for(int order=1;order<=maxOrder;order++) {
+				double angle = minAngle+angleIncr*step;
+				double x = Math.sin( order*angle/2);
+				harmonics[step][order] = x*x;
+			}
+			// Rotate for next step
+			axis.rotate(ca2, angleIncr);
+		}
+
+		Matrix y = new Matrix(distances);
+		Matrix M = new Matrix(harmonics);
+		//y=y.getMatrix(1, steps-1, 0, 0);
+		//M=M.getMatrix(1, steps-1, 1, maxOrder-1);
+
+
+		Matrix A = M.transpose().times(M).times(2);
+		Matrix b = M.transpose().times(y).times(2);
+		Matrix c = y.transpose().times(y);
+
+		// f(x) = x'Ax/2-bx+c
+		// f'(x) = Ax-b = 0
+		// Ax = b
+		//A.inverse();
+
+		Matrix harmonicWeights = A.solve(b);
 
 		return harmonicWeights.transpose().getArray()[0];
 	}
-	
+
 	public static void main(String[] args) {
 		String name;
 		name = "d1ijqa1";
-//		name = "1G6S";
+		//		name = "1G6S";
 		name = "1MER.A";
-//		name = "1MER";
-//		name = "1TIM.A";
-//		name = "d1h70a_";
+		//		name = "1MER";
+		//		name = "1TIM.A";
+		//		name = "d1h70a_";
 		name = "2YMS";
 		try {
-			
+
 			// Perform alignment to determine axis
 			Atom[] ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
 			Atom[] ca2 = StructureTools.cloneCAArray(ca1);
@@ -193,7 +295,7 @@ public class RotationOrderDetector implements OrderDetector {
 
 			// Search for orders up to 9
 			RotationOrderDetector detector = new RotationOrderDetector(9);
-			
+
 			// Calculate order
 			int order = detector.calculateOrder(alignment, ca1);
 			System.out.println("Order: "+order);
@@ -202,7 +304,7 @@ public class RotationOrderDetector implements OrderDetector {
 			RotationAxis axis = new RotationAxis(alignment);
 			double[] harmonics = detector.fitHarmonics(ca1,axis);
 			System.out.println("Order Weights: "+Arrays.toString(harmonics));
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (StructureException e) {
@@ -210,8 +312,8 @@ public class RotationOrderDetector implements OrderDetector {
 		} catch (OrderDetectionFailedException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
+
+
+
 	}
 }
--- src/test/java/org/biojava3/structure/align/symm/order/RotationOrderDetectorTest.java
+++ src/test/java/org/biojava3/structure/align/symm/order/RotationOrderDetectorTest.java
@@ -11,7 +11,6 @@ import org.biojava.bio.structure.Atom;
 import org.biojava.bio.structure.StructureException;
 import org.biojava.bio.structure.StructureTools;
 import org.biojava.bio.structure.align.model.AFPChain;
-import org.biojava.bio.structure.align.util.AtomCache;
 import org.biojava.bio.structure.align.util.RotationAxis;
 import org.biojava.bio.structure.scop.ScopFactory;
 import org.biojava3.structure.align.symm.CeSymm;
@@ -56,7 +55,7 @@ public class RotationOrderDetectorTest {
 		axis = new RotationAxis(alignment);
 
 		coefs = detector.fitHarmonics(ca1, axis);
-		expectedHarmonics = new double[] {
+		expectedHarmonics = new double[] { 0,
 				1.218482, 2.110836, 0.7203669, 0.8226358,
 				0.6092911, 0.6339138, 0.4439472, 0.4737434,
 		};
@@ -73,12 +72,154 @@ public class RotationOrderDetectorTest {
 		axis = new RotationAxis(alignment);
 
 		coefs = detector.fitHarmonics(ca1, axis);
-		expectedHarmonics = new double[] {
+		expectedHarmonics = new double[] { 0,
 				0.5176411, 0.5359353, 0.4928912, 0.5044149,
 				0.4031307, 1.915722, 0.4049375, 0.4456366
 		};
 
 		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+	}
+	@Test
+	public void testCalculateOrderByHarmonics() throws IOException, StructureException, OrderDetectionFailedException {
+		String name;
+
+		// Perform alignment to determine axis
+		Atom[] ca1, ca2;
+		AFPChain alignment;
+		int order;
+
+		name = "1MER.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+
+		order = detector.calculateOrderHarmonics(alignment, ca1);
+
+		assertEquals(name,2,order);
+
+
+		ce = new CeSymm();// work around bug
+
+		name = "d1ijqa1";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+
+		order = detector.calculateOrderHarmonics(alignment, ca1);
+
+		assertEquals(name,6,order);
+
+		ce = new CeSymm();// work around bug
+
+		name = "1TIM.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+
+		order = detector.calculateOrderHarmonics(alignment, ca1);
+
+		assertEquals(name,1,order);// tough case
+	}
+
+	@Test
+	public void testFitHarmonicsFloating() throws IOException, StructureException {
+		String name;
+
+		// Perform alignment to determine axis
+		Atom[] ca1, ca2;
+		AFPChain alignment;
+		RotationAxis axis;
+		double[] coefs,expectedHarmonics;
+
+		name = "1MER.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.fitHarmonicsFloating(ca1, axis);
+		expectedHarmonics = new double[] { 2.287581,
+				0.6300994, 1.51542, 0.1546644, 0.2601547,
+				 0.08553783, 0.1208464, -0.02471022, 0.01952981
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+
+		ce = new CeSymm();// work around bug
+
+		name = "d1ijqa1";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.fitHarmonicsFloating(ca1, axis);
+		expectedHarmonics = new double[] { 1.779942,
+				0.09846244, 0.1032024, 0.07649375, 0.07587491,
+				-0.007642349, 1.494255, 0.002693317, 0.03414484
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+		
+		ce = new CeSymm();// work around bug
+
+		name = "1TIM.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.fitHarmonicsFloating(ca1, axis);
+		expectedHarmonics = new double[] { 2.386711,
+				0.3044205, 0.2654955, 0.2251938, 0.2217246,
+				0.05211578, 0.2773498, 0.046725, 0.2101539
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+	}
+	@Test
+	public void testCalculateOrderByHarmonicsFloating() throws IOException, StructureException, OrderDetectionFailedException {
+		String name;
+
+		// Perform alignment to determine axis
+		Atom[] ca1, ca2;
+		AFPChain alignment;
+		int order;
+
+		name = "1MER.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+
+		order = detector.calculateOrderHarmonicsFloating(alignment, ca1);
+
+		assertEquals(name,2,order);
+
+
+		ce = new CeSymm();// work around bug
+
+		name = "d1ijqa1";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+
+		order = detector.calculateOrderHarmonicsFloating(alignment, ca1);
+
+		assertEquals(name,6,order);
+
+		ce = new CeSymm();// work around bug
+
+		name = "1TIM.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+
+		order = detector.calculateOrderHarmonicsFloating(alignment, ca1);
+
+		assertEquals(name,6,order);// tough case
 
 	}
 

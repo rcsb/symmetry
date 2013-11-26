6086e23a58607117c009562052849ae8566e85f5
Spencer Bliven <sbliven@ucsd.edu>
2013-11-25 17:50:58 -0800

    Adding cusp code

--- src/main/java/org/biojava3/structure/align/symm/order/RotationOrderDetector.java
+++ src/main/java/org/biojava3/structure/align/symm/order/RotationOrderDetector.java
@@ -416,6 +416,129 @@ public class RotationOrderDetector implements OrderDetector {
 
 		return sses;
 	}
+	double[] trySingleCuspByAmp(Atom[] ca, RotationAxis axis) throws StructureException {
+		// Range of angles to use for training
+		final double minAngle = Math.floor(Math.PI/maxOrder/angleIncr)*angleIncr; // first valid peak
+		final double maxAngle = Math.PI;
+		// Number of angle steps
+		final int steps = (int)Math.floor((maxAngle-minAngle)/angleIncr);
+
+		// Fit (angle,distance) points to the following equation
+		// f(angle) = a0 + a1*sin^2(1*angle/2) + a2*sin^2(2*angle/2) +...+ a*sin2(maxOrder*angle/2)
+		// goal is to find a_1...a_maxOrder
+
+		double[][] distances = new double[steps][1];//preserve matrix dimensions
+		Atom[] ca2 = StructureTools.cloneCAArray(ca);
+		if(minAngle != 0.) {
+			axis.rotate(ca2, minAngle);
+		}
+		for (int step=0; step<steps;step++) {
+			double dist = superpositionDistance(ca, ca2);
+			distances[step][0] = dist;
+			// Rotate for next step
+			axis.rotate(ca2, angleIncr);
+		}
+
+		double[] amplitudes = new double[maxOrder];
+
+		for( int order=1;order <= maxOrder; order++) {
+
+			// holds the sin2(i*angle/2) terms
+			double[][] harmonics = new double[steps][2];
+
+			for (int step=0; step<steps;step++) {
+
+				//initialize intercept column
+				harmonics[step][0] = 1.;
+
+				//order-dependent column
+				double angle = minAngle+angleIncr*step;
+				double x = Math.sqrt( 1 - Math.cos(order*angle) );
+				harmonics[step][1] = x;
+			}
+
+			Matrix y = new Matrix(distances);
+			Matrix M = new Matrix(harmonics);
+
+			Matrix A = M.transpose().times(M).times(2);
+			Matrix b = M.transpose().times(y).times(2);
+			//Matrix c = y.transpose().times(y);
+
+			// f(x) = x'Ax/2-bx+c
+			// f'(x) = Ax-b = 0
+			// Ax = b
+
+			Matrix harmonicWeights = A.solve(b);
+
+			amplitudes[order-1] = harmonicWeights.get(1, 0);
+		}
+		
+		return amplitudes;
+	}
+	double[] trySingleCuspBySSE(Atom[] ca, RotationAxis axis) throws StructureException {
+		// Range of angles to use for training
+		final double minAngle = Math.floor(Math.PI/maxOrder/angleIncr)*angleIncr; // first valid peak
+		final double maxAngle = Math.PI;
+		// Number of angle steps
+		final int steps = (int)Math.floor((maxAngle-minAngle)/angleIncr);
+
+		// Fit (angle,distance) points to the following equation
+		// f(angle) = a0 + a1*sin^2(1*angle/2) + a2*sin^2(2*angle/2) +...+ a*sin2(maxOrder*angle/2)
+		// goal is to find a_1...a_maxOrder
+
+		double[][] distances = new double[steps][1];//preserve matrix dimensions
+		Atom[] ca2 = StructureTools.cloneCAArray(ca);
+		if(minAngle != 0.) {
+			axis.rotate(ca2, minAngle);
+		}
+		for (int step=0; step<steps;step++) {
+			double dist = superpositionDistance(ca, ca2);
+			distances[step][0] = dist;
+			// Rotate for next step
+			axis.rotate(ca2, angleIncr);
+		}
+
+		double[] sses = new double[maxOrder];
+
+		for( int order=1;order <= maxOrder; order++) {
+
+			// holds the sin2(i*angle/2) terms
+			double[][] harmonics = new double[steps][2];
+
+			for (int step=0; step<steps;step++) {
+
+				//initialize intercept column
+				harmonics[step][0] = 1.;
+
+				//order-dependent column
+				double angle = minAngle+angleIncr*step;
+				double x = Math.sqrt( 1 - Math.cos(order*angle) );
+				harmonics[step][1] = x*x;
+			}
+
+			Matrix y = new Matrix(distances);
+			Matrix M = new Matrix(harmonics);
+
+			Matrix A = M.transpose().times(M).times(2);
+			Matrix b = M.transpose().times(y).times(2);
+			//Matrix c = y.transpose().times(y);
+
+			// f(x) = x'Ax/2-bx+c
+			// f'(x) = Ax-b = 0
+			// Ax = b
+
+			Matrix harmonicWeights = A.solve(b);
+
+			Matrix predictions = M.times(harmonicWeights);
+			predictions.minusEquals(y);//errors
+			Matrix sse = predictions.transpose().times(predictions);
+
+			// Calculate RSSE (could save some arithmetic, but this is easier to compare)
+			sses[order-1] = Math.sqrt(sse.get(0, 0)/steps);
+		}
+
+		return sses;
+	}
 
 	public static void main(String[] args) {
 		String name;
--- src/test/java/org/biojava3/structure/align/symm/order/RotationOrderDetectorTest.java
+++ src/test/java/org/biojava3/structure/align/symm/order/RotationOrderDetectorTest.java
@@ -24,7 +24,7 @@ import org.junit.Test;
 public class RotationOrderDetectorTest {
 	private RotationOrderDetector detector;
 	private CeSymm ce;
-	
+
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -34,7 +34,7 @@ public class RotationOrderDetectorTest {
 
 		ce = new CeSymm();
 		detector = new RotationOrderDetector(8);
-		
+
 		ScopFactory.setScopDatabase(ScopFactory.VERSION_1_75A);
 	}
 
@@ -140,7 +140,7 @@ public class RotationOrderDetectorTest {
 		coefs = detector.fitHarmonicsFloating(ca1, axis);
 		expectedHarmonics = new double[] { 2.287581,
 				0.6300994, 1.51542, 0.1546644, 0.2601547,
-				 0.08553783, 0.1208464, -0.02471022, 0.01952981
+				0.08553783, 0.1208464, -0.02471022, 0.01952981
 		};
 
 		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
@@ -161,7 +161,7 @@ public class RotationOrderDetectorTest {
 		};
 
 		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
-		
+
 		ce = new CeSymm();// work around bug
 
 		name = "1TIM.A";
@@ -174,90 +174,6 @@ public class RotationOrderDetectorTest {
 		expectedHarmonics = new double[] { 2.386711,
 				0.3044205, 0.2654955, 0.2251938, 0.2217246,
 				0.05211578, 0.2773498, 0.046725, 0.2101539
-		};
-
-		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
-
-	}
-	
-	@Test
-	public void testTrySingleHarmonicsFloatingByAmp() throws IOException, StructureException {
-		String name;
-
-		// Perform alignment to determine axis
-		Atom[] ca1, ca2;
-		AFPChain alignment;
-		RotationAxis axis;
-		double[] coefs,expectedHarmonics;
-
-		name = "1MER.A";
-		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
-		ca2 = StructureTools.cloneCAArray(ca1);
-		alignment = ce.align(ca1, ca2);
-		axis = new RotationAxis(alignment);
-
-		coefs = detector.trySingleHarmonicsFloatingByAmp(ca1, axis);
-		expectedHarmonics = new double[] { 
-				0.1739415, 1.246599, -0.3484241, -0.2464376,
-				-0.2841697, -0.2528177, -0.250818, -0.2106903
-		};
-
-		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
-
-
-		ce = new CeSymm();// work around bug
-
-		name = "d1ijqa1";
-		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
-		ca2 = StructureTools.cloneCAArray(ca1);
-		alignment = ce.align(ca1, ca2);
-		axis = new RotationAxis(alignment);
-
-		coefs = detector.trySingleHarmonicsFloatingByAmp(ca1, axis);
-		expectedHarmonics = new double[] { 
-				-0.1288947, -0.2341662, -0.1283006, -0.2218122,
-				-0.1814559, 1.445211, -0.1186493, -0.163856
-		};
-
-		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
-		
-		ce = new CeSymm();// work around bug
-
-		name = "1TIM.A";
-		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
-		ca2 = StructureTools.cloneCAArray(ca1);
-		alignment = ce.align(ca1, ca2);
-		axis = new RotationAxis(alignment);
-
-		coefs = detector.trySingleHarmonicsFloatingByAmp(ca1, axis);
-		expectedHarmonics = new double[] {
-				0.086517, 0.01248985, 0.01258807, -0.001225424,
-				-0.1398931, 0.117046, -0.08244989, 0.1015177
-		};
-
-		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
-
-	}
-	@Test
-	public void testTrySingleHarmonicsFloatingBySSE() throws IOException, StructureException {
-		String name;
-
-		// Perform alignment to determine axis
-		Atom[] ca1, ca2;
-		AFPChain alignment;
-		RotationAxis axis;
-		double[] coefs,expectedHarmonics;
-
-		name = "1MER.A";
-		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
-		ca2 = StructureTools.cloneCAArray(ca1);
-		alignment = ce.align(ca1, ca2);
-		axis = new RotationAxis(alignment);
-
-		coefs = detector.trySingleHarmonicsFloatingBySSE(ca1, axis);
-		expectedHarmonics = new double[] { 
-				0.4411113, 0.1688462, 0.4289372, 0.436751,
-				0.4336008, 0.4358288, 0.4358056, 0.4384935
 		};
 
 		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
@@ -303,6 +219,177 @@ public class RotationOrderDetectorTest {
 		order = detector.calculateOrderHarmonicsFloating(alignment, ca1);
 
 		assertEquals(name,1,order);// tough case
+
+	}
+
+
+	@Test
+	public void testTrySingleHarmonicsFloatingByAmp() throws IOException, StructureException {
+		String name;
+
+		// Perform alignment to determine axis
+		Atom[] ca1, ca2;
+		AFPChain alignment;
+		RotationAxis axis;
+		double[] coefs,expectedHarmonics;
+
+		name = "1MER.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.trySingleHarmonicsFloatingByAmp(ca1, axis);
+		expectedHarmonics = new double[] { 
+				0.1739415, 1.246599, -0.3484241, -0.2464376,
+				-0.2841697, -0.2528177, -0.250818, -0.2106903
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+
+		ce = new CeSymm();// work around bug
+
+		name = "d1ijqa1";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.trySingleHarmonicsFloatingByAmp(ca1, axis);
+		expectedHarmonics = new double[] { 
+				-0.1288947, -0.2341662, -0.1283006, -0.2218122,
+				-0.1814559, 1.445211, -0.1186493, -0.163856
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+		ce = new CeSymm();// work around bug
+
+		name = "1TIM.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.trySingleHarmonicsFloatingByAmp(ca1, axis);
+		expectedHarmonics = new double[] {
+				0.086517, 0.01248985, 0.01258807, -0.001225424,
+				-0.1398931, 0.117046, -0.08244989, 0.1015177
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+	}
+	@Test
+	public void testTrySingleHarmonicsFloatingBySSE() throws IOException, StructureException {
+		String name;
+
+		// Perform alignment to determine axis
+		Atom[] ca1, ca2;
+		AFPChain alignment;
+		RotationAxis axis;
+		double[] coefs,expectedHarmonics;
+
+		name = "1MER.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.trySingleHarmonicsFloatingBySSE(ca1, axis);
+		expectedHarmonics = new double[] { 
+				0.4411113, 0.1688462, 0.4289372, 0.436751,
+				0.4336008, 0.4358288, 0.4358056, 0.4384935
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+	}
+
+	
+
+	@Test
+	public void testTrySingleCuspByAmp() throws IOException, StructureException {
+		String name;
+
+		// Perform alignment to determine axis
+		Atom[] ca1, ca2;
+		AFPChain alignment;
+		RotationAxis axis;
+		double[] coefs,expectedHarmonics;
+
+		name = "1MER.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.trySingleCuspByAmp(ca1, axis);
+		expectedHarmonics = new double[] { 
+				0.3485579, 1.079108, -0.3344876, -0.2502484,
+				-0.2343293, -0.2120265, -0.1883948, -0.166058
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+
+		ce = new CeSymm();// work around bug
+
+		name = "d1ijqa1";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.trySingleCuspByAmp(ca1, axis);
+		expectedHarmonics = new double[] { 
+				-0.172995, -0.1505527, 0.1552747, -0.2200142,
+				-0.1770285, 1.18373, -0.1158369, -0.1520574
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+		ce = new CeSymm();// work around bug
+
+		name = "1TIM.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.trySingleCuspByAmp(ca1, axis);
+		expectedHarmonics = new double[] {
+				0.08433796, 0.02381816, 0.02802065, 0.01412526,
+				-0.1208657, 0.0874411, -0.07040143, 0.08127973
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
+
+	}
+	@Test
+	public void testTrySingleCuspBySSE() throws IOException, StructureException {
+		String name;
+
+		// Perform alignment to determine axis
+		Atom[] ca1, ca2;
+		AFPChain alignment;
+		RotationAxis axis;
+		double[] coefs,expectedHarmonics;
+
+		name = "1MER.A";
+		ca1 = StructureTools.getAtomCAArray(StructureTools.getStructure(name));
+		ca2 = StructureTools.cloneCAArray(ca1);
+		alignment = ce.align(ca1, ca2);
+		axis = new RotationAxis(alignment);
+
+		coefs = detector.trySingleCuspBySSE(ca1, axis);
+		expectedHarmonics = new double[] { 
+				0.4411113, 0.1688462, 0.4289372, 0.436751,
+				0.4336008, 0.4358288, 0.4358056, 0.4384935
+		};
+
+		assertArrayEquals(name,expectedHarmonics,coefs,1e-4);
 
 	}
 

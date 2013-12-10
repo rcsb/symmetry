dd5f58b9de9d4a0d0502fb0e083f83ab4888564f
dmyersturnbull <dmyersturnbull@gmail.com>
2013-12-09 20:19:35 -0800

    Simple improvements to ConservedSequenceFinder.

--- src/main/java/org/biojava3/structure/align/symm/census2/analysis/ConservedSequenceFinder.java
+++ src/main/java/org/biojava3/structure/align/symm/census2/analysis/ConservedSequenceFinder.java
@@ -1,10 +1,13 @@
 package org.biojava3.structure.align.symm.census2.analysis;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
 
 import org.biojava.bio.structure.Atom;
-import org.biojava.bio.structure.StructureException;
 import org.biojava.bio.structure.StructureTools;
 import org.biojava.bio.structure.align.util.AtomCache;
 import org.biojava3.alignment.Alignments;
@@ -17,8 +20,8 @@ import org.biojava3.core.sequence.ProteinSequence;
 import org.biojava3.core.sequence.compound.AminoAcidCompound;
 import org.biojava3.structure.align.symm.census2.Result;
 import org.biojava3.structure.align.symm.census2.Results;
+import org.biojava3.structure.align.symm.census2.SignificanceFactory;
 import org.biojava3.structure.align.symm.protodomain.Protodomain;
-import org.biojava3.structure.align.symm.protodomain.ProtodomainCreationException;
 
 /**
  * Find sequence motifs that correspond to symmetry subunits or protodomains.
@@ -27,61 +30,74 @@ import org.biojava3.structure.align.symm.protodomain.ProtodomainCreationExceptio
 public class ConservedSequenceFinder {
 
 	public static void main(String[] args) throws IOException {
-		if (args.length < 1 || args.length > 2) {
+		if (args.length < 2 || args.length > 3) {
 			System.err.println("Usage: " + ConservedSequenceFinder.class.getSimpleName() + " input-census-file.xml [min-score]");
 			return;
 		}
-		float minScore = 0.8f;
-		if (args.length > 1) {
+		float minScore = 0.7f;
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(new File(args[1]))));
+		if (args.length > 2) {
 			minScore = Float.parseFloat(args[1]);
 		}
 		ConservedSequenceFinder finder = new ConservedSequenceFinder(new AtomCache());
-		finder.find(Results.fromXML(new File(args[0])), minScore);
+		finder.find(Results.fromXML(new File(args[0])), minScore, pw);
+		pw.close();
 	}
-	
+
 	private AtomCache cache;
-	
+
 	public ConservedSequenceFinder(AtomCache cache) {
 		super();
 		this.cache = cache;
 	}
 
-	public void find(Results results, float maxDistance) {
+	public void find(Results results, float maxDistance, PrintWriter pw) {
+		
+		Collections.shuffle(results.getData()); // shuffle!
+		
 		for (Result result : results.getData()) {
-			if (result.getOrder() != 2) continue;
+			
+			if (!SignificanceFactory.rotationallySymmetricSmart().isSignificant(result)) continue;
+			
+			if (result.getOrder() == null || result.getOrder() != 2) continue;
+			
 			Protodomain wholeAligned, protodomain1, protodomain2;
+			
 			try {
+				
 				wholeAligned = Protodomain.fromString(result.getProtodomain(), result.getScopId(), cache);
 				protodomain1 = wholeAligned.createSubstruct(result.getOrder(), 0).spliceApproxConsecutive();
 				protodomain2 = wholeAligned.createSubstruct(result.getOrder(), 1).spliceApproxConsecutive();
-				System.out.println(wholeAligned);
-				System.out.println(protodomain1);
-				System.out.println(protodomain2);
-				System.out.println();
-			} catch (ProtodomainCreationException e) {
-				e.printStackTrace();
-				continue;
-			}
-			try {
 				protodomain1.buildStructure();
 				protodomain2.buildStructure();
-			} catch (IOException e) {
+				
+				Atom[] ca1 = StructureTools.getAtomCAArray(protodomain1.getStructure());
+				Atom[] ca2 = StructureTools.getAtomCAArray(protodomain2.getStructure());
+				
+				ProteinSequence seq1 = new ProteinSequence(StructureTools.convertAtomsToSeq(ca1));
+				ProteinSequence seq2 = new ProteinSequence(StructureTools.convertAtomsToSeq(ca2));
+				
+				PairwiseSequenceAligner<ProteinSequence, AminoAcidCompound> aligner = Alignments.getPairwiseAligner(seq1, seq2, PairwiseSequenceAlignerType.LOCAL, new SimpleGapPenalty(), SubstitutionMatrixHelper.getBlosum62());
+				SequencePair<ProteinSequence, AminoAcidCompound> pair = aligner.getPair();
+//				System.err.println(aligner.getDistance());
+				
+				// TODO we are really only interested in motifs in the equivalent residues in the protodomains
+				if (aligner.getDistance() > maxDistance) continue;
+				
+				pw.println(result.getScopId());
+				pw.println(protodomain1);
+				pw.println(protodomain2);
+				
+				pw.println(pair.toString(80));
+				pw.println("score: " + aligner.getScore());
+
+				pw.println("-----------------------------------------------------");
+				
+			} catch (Exception e) {
 				e.printStackTrace();
-				continue;
-			} catch (StructureException e) {
-				e.printStackTrace();
-				continue;
+			} finally {
 			}
-			Atom[] ca1 = StructureTools.getAtomCAArray(protodomain1.getStructure());
-			Atom[] ca2 = StructureTools.getAtomCAArray(protodomain2.getStructure());
-			ProteinSequence seq1 = new ProteinSequence(StructureTools.convertAtomsToSeq(ca1));
-			ProteinSequence seq2 = new ProteinSequence(StructureTools.convertAtomsToSeq(ca2));
-			PairwiseSequenceAligner<ProteinSequence, AminoAcidCompound> aligner = Alignments.getPairwiseAligner(seq1, seq2, PairwiseSequenceAlignerType.LOCAL, new SimpleGapPenalty(), SubstitutionMatrixHelper.getBlosum62());
-			SequencePair<ProteinSequence, AminoAcidCompound> pair = aligner.getPair();
-			if (aligner.getDistance() < maxDistance) continue;
-			// TODO we are really only interested in motifs in the equivalent residues in the protodomains
-			System.out.println(pair.toString(80));
 		}
 	}
-	
+
 }

8087b6c79c24e2967e81763e35276aadce0311d8
Spencer Bliven <sbliven@ucsd.edu>
2014-03-12 12:54:36 +0100

    Fix CeSymmMainTest to reflect the sample input file
    This should have been included in a9ee5b6e0ed512e667b7e29c62b1a23c1ab7d6b5

--- src/test/java/demo/CeSymmMainTest.java
+++ src/test/java/demo/CeSymmMainTest.java
@@ -8,22 +8,24 @@ import java.util.List;
 
 import org.junit.Test;
 
+import demo.CeSymmMain;
+
 public class CeSymmMainTest {
 
 	@Test
 	public void testParseInput() throws FileNotFoundException {
-		URL url = CeSymmMainTest.class.getResource("/inputtest.txt");
+		URL url = CeSymmMainTest.class.getResource("/cesymmtest.txt");
 		String filename = url.getFile();
 		List<String> names = CeSymmMain.parseInputStructures(filename);
 		
 		assertEquals(6,names.size());
 		int i = 0;
-		assertEquals("line1",names.get(i++));
-		assertEquals("line2.1",names.get(i++));
-		assertEquals("2.2",names.get(i++));
-		assertEquals("twothree",names.get(i++));
-		assertEquals("three",names.get(i++));
-		assertEquals("four",names.get(i++));
+		assertEquals("d1ijqa1",names.get(i++));
+		assertEquals("1G6S",names.get(i++));
+		assertEquals("1MER.A",names.get(i++));
+		assertEquals("d1h70a_",names.get(i++));
+		assertEquals("2YMS_A:,C:,B:,D:",names.get(i++));
+		assertEquals("1HIV",names.get(i++));
 	}
 
 }

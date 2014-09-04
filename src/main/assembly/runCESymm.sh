41a1208b67f45b3904c91ebd3bc08b3b534c4157
Spencer Bliven <sbliven@ucsd.edu>
2014-09-04 10:39:39 +0200

    Fixing bug in how runCESymm handles non-symlink paths
    (Broken in 5ab41267)

--- src/main/assembly/runCESymm.sh
+++ src/main/assembly/runCESymm.sh
@@ -44,6 +44,13 @@
 ### Execute jar ###
 
 
+# Get the base directory of the argument.
+# Can resolve single symlinks if readlink is installed
+function scriptdir {
+    cd "$(dirname "$1")"
+    cd "$(dirname "$(readlink "$1" 2>/dev/null || basename "$1" )")"
+    pwd
+}
+DIR="$(scriptdir "$0" )"
 # send the arguments to the java app
-DIR="$(dirname "$(readlink "$0")")"
 java -Xmx500M -jar "$DIR/${project.build.finalName}.jar" "$@"

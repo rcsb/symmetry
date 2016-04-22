4f4907710442d7ef2b87a12012162828326659a2
lafita <aleixlafita@gmail.com>
2016-04-22 12:58:48 +0200

    Factor out the CeSymmWorker into a separate class

---
+++ symmetry-tools/src/main/java/workers/CeSymmWorker.java
@@ -0,0 +1,91 @@
+package workers;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.biojava.nbio.structure.Atom;
+import org.biojava.nbio.structure.Structure;
+import org.biojava.nbio.structure.StructureException;
+import org.biojava.nbio.structure.StructureIdentifier;
+import org.biojava.nbio.structure.StructureTools;
+import org.biojava.nbio.structure.align.util.AtomCache;
+import org.biojava.nbio.structure.symmetry.gui.SymmetryDisplay;
+import org.biojava.nbio.structure.symmetry.internal.CESymmParameters;
+import org.biojava.nbio.structure.symmetry.internal.CeSymm;
+import org.biojava.nbio.structure.symmetry.internal.CeSymmResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import writers.CeSymmWriter;
+
+/**
+ * This Runnable implementation runs CeSymm on the input structure and with the
+ * input parameters and writes the results to the output writers provided.
+ * <p>
+ * If the 3D visualization is turned on, it creates a new thread with the Jmol
+ * frame.
+ * 
+ * @author Aleix Lafita
+ *
+ */
+public class CeSymmWorker implements Runnable {
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(CeSymmWorker.class);
+
+	private StructureIdentifier id;
+	private CESymmParameters params;
+	private AtomCache cache;
+	private List<CeSymmWriter> writers;
+	private boolean show3d;
+
+	public CeSymmWorker(StructureIdentifier id, CESymmParameters params,
+			AtomCache cache, List<CeSymmWriter> writers, boolean show3d) {
+		this.id = id;
+		this.cache = cache;
+		this.writers = writers;
+		this.show3d = show3d;
+		this.params = params;
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			// Obtain the structure representation
+			Atom[] atoms;
+			try {
+				Structure s = cache.getStructure(id);
+				atoms = StructureTools.getRepresentativeAtomArray(s);
+			} catch (IOException | StructureException e) {
+				logger.error("Could not load Structure " + id.getIdentifier(),
+						e);
+				return;
+			}
+			// Run the symmetry analysis
+			CeSymmResult result = CeSymm.analyze(atoms, params);
+
+			// Write into the output files
+			for (CeSymmWriter writer : writers) {
+				try {
+					synchronized (writer) {
+						writer.writeResult(result);
+					}
+				} catch (Exception e) {
+					logger.error(
+							"Could not save results for " + id.getIdentifier(),
+							e);
+				}
+			}
+
+			// Display alignment in 3D Jmol
+			if (show3d) {
+				SymmetryDisplay.display(result);
+			}
+		} catch (Exception e) {
+			logger.error("Could not complete job: " + id.getIdentifier(), e);
+		} finally {
+			logger.info("Finished job: " + id);
+		}
+	}
+}
